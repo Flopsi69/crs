32f506c9a6b96bac9424241636d1e16843f7b3f5
Alex <flopsi69@gmail.com>
2021-02-15 13:50:35 +0200

    fix

--- modernbeds/index.js
+++ modernbeds/index.js
@@ -35,7 +35,7 @@ let stylesList = `
 }
 @media only screen and (max-width: 767px) {
   .product-template__container .product-row {
-      margin: 0;
+      margin: 0 -15px;
   }
 }
 .slick-current .go-option-inner{
@@ -45,6 +45,37 @@ let stylesList = `
 .slick-arrow.hs-loading-btn {
   position: absolute;
 }
+.product-form .swatch .swatch-element {
+  display: flex;
+  margin: 4px;
+}
+.product-form {
+  margin: 0;
+}
+.go-slider {
+  padding: 0 10px;
+}
+.go-slider .slick-arrow {
+  background: none;
+  height: 30px;
+  width: 20px;
+}
+.go-slider .slick-prev {
+  left: -13px!important;
+}
+.go-slider .slick-next{
+  right: -13px!important;
+}
+.go-slider .slick-prev:before, .go-slider .slick-next:before {
+  font-size: 34px;
+}
+.product-form .swatch .swatch-element:hover .tooltip-label {
+  display: none;
+}
+.go-options-slider {
+  margin-top: 4px;
+}
+
 `;
 
 // connect to DOM
@@ -55,12 +86,8 @@ document.body.appendChild(styles);
 
 
 /*HTML insert start */
-// const swatch = `
-//   <div class="swatch__list"></div>
-// `;
-
 const swatchEl = document.querySelector(".swatch");
-swatchEl.insertAdjacentHTML("afterbegin", "<div class='swatch__list'></div>");
+swatchEl.insertAdjacentHTML("beforeend", "<div class='swatch__list go-slider'></div>");
 document.querySelectorAll(".swatch-element").forEach(function (el) {
   document.querySelector(".swatch__list").insertAdjacentElement("beforeend", el);
 })
@@ -69,11 +96,16 @@ $(".swatch__list").slick({
   slidesToShow: 5,
 })
 
+$(".slick-arrow").on("click", function (e) {
+  e.preventDefault();
+  e.stopPropagation();
+})
+
 document.querySelectorAll(".mw-options-container .mw-option-select").forEach(function (el) {
   let labelText = el.querySelector("label").innerText.toLocaleLowerCase();
   let selectEl = el.querySelector("select");
   selectEl.style.display = "none";
-  selectEl.insertAdjacentHTML("beforebegin", "<div class='go-options-slider'></div>");
+  selectEl.insertAdjacentHTML("beforebegin", "<div class='go-options-slider go-slider'></div>");
   Array.from(selectEl.options).forEach(function (option, i) {
     if (i == 0) return;
     let optionTextRaw = option.innerText.match(/(.*)\s\((.*)\)/);
@@ -100,24 +132,33 @@ document.querySelectorAll(".mw-options-container .mw-option-select").forEach(fun
   $(".slick-arrow").on("click", function (e) {
     e.preventDefault();
     e.stopPropagation();
-    console.log("clicked")
   })
 
-  // size, storage, mattress, headboard, reinforce, headboard height
+   // size, storage, mattress, headboard, reinforce, headboard height
 })
 
 
-var deliveryEl = searchText($(".page-width h2"), "delivery");
-var deliveryBlock = extractHtml(deliveryEl);
+let deliveryEl = searchText($(".page-width h2"), "delivery");
+if (deliveryEl) {
+  let deliveryBlockEl = extractHtmltoBlock(deliveryEl);
+}
 
-var returnEl = searchText($(".page-width h2"), "return");
+let returnEl = searchText($(".page-width h2"), "return");
+if (returnEl) {
+  let reutrnBlockEl = extractHtmltoBlock(returnEl);
+}
+
+let dimensions = searchText($(".page-width h4"), "dimensions");
+if (returnEl) {
+  let reutrnBlockEl = extractHtmltoBlock(returnEl);
+}
+
 
 
 function searchText(list, searchWord) {
   let findedEl;
   list.each(function (i, el) {
-    console.log(searchWord, $(el).text().toLocaleLowerCase().trim() == searchWord);
-    if ($(el).text().toLocaleLowerCase().trim() != searchWord) return;
+    if ($(el).text().trim().split(" ")[0].toLocaleLowerCase() != searchWord) return;
     findedEl = el;
     return false;
   })
@@ -125,13 +166,27 @@ function searchText(list, searchWord) {
   return findedEl ? findedEl : false;
 }
 
-function extractHtml(element) {
-  let fromEl = element;
-  while(fromEl) {
-    fromEl = fromEl.nextElementSibling;
-    if (fromEl.tagName == "DIV" || fromEl.tagName == "ul" || fromEl.tagName == "span")
-    console.log(fromEl);
+function extractHtmltoBlock(element) {
+  let blockEl = document.createElement("div");
+  blockEl.classList.add("go-accordion");
+  blockEl.insertAdjacentHTML("beforeend", `<h2 class='go-accordion__head'>${element.innerText}</h2>`);
+  blockEl.insertAdjacentHTML("beforeend", `<div class='go-accordion__body'></div>`);
+  let currentEl = element;
+  while (currentEl) {
+    currentEl = currentEl.nextElementSibling;
+
+    if (!currentEl) break;
+
+    let currentElTag = currentEl.tagName.toLocaleLowerCase();
+
+    if (currentElTag == "div" || currentElTag == "ul" || currentElTag == "span" || currentElTag == "p") {
+      blockEl.querySelector(".go-accordion__body").insertAdjacentElement("beforeend", currentEl.cloneNode(true));
+    } else {
+      break;
+    }
   }
+  console.log(blockEl);
+  return blockEl;
 }
 
 
