c25037ca4c629ab355fe1a6a84ab34e8dcb3dc2f
Alex <flopsi69@gmail.com>
2021-03-29 11:32:33 +0300

    fix

--- modernbeds/checkout/index.js
+++ modernbeds/checkout/index.js
@@ -445,11 +445,11 @@ styles.innerHTML = stylesList;
 /*** HTML insert start ***/
 
 /*** HTML insert -end- ***/
-document.addEventListener('DOMContentLoaded', function () {
-  setTimeout(init, 1200);
-});
+// document.addEventListener('DOMContentLoaded', function () {
+setTimeout(init, 1200);
+// });
 
-function init() 
+function init() {
   document.body.appendChild(styles);
 
   changeMinor();
