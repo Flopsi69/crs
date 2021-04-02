36c85197a40d77a60b200447d0f39c86458ad537
Alex <flopsi69@gmail.com>
2021-04-02 13:12:01 +0300

    fix

--- depositPhotos/licenses/index-c.js
+++ depositPhotos/licenses/index-c.js
@@ -40,10 +40,8 @@ const REPO_DIR = 'https://flopsi69.github.io/crs/depositPhotos/licenses';
 /*** STYLES insert start ***/
 
 let stylesList = `
-  @media screen and (min-width: 1441px) and (max-height: 800px) {
-    .file-view-upgrade__top-box { padding: 0; }
-  }
   @media screen and (min-width: 1441px) {
+    .file-view-upgrade__top-box { padding: 0!important; }
     .file-view-page-upgrade_image .file-view-upgrade__content-box {
       max-width: 100%;
     }
