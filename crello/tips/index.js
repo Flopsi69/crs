d208abd3e1ee826ab03ccac40fad6b91cf1ae209
Alex <flopsi69@gmail.com>
2021-04-21 21:08:52 +0300

    fix

--- crello/tips/index.js
+++ crello/tips/index.js
@@ -46,6 +46,35 @@
 const REPO_DIR = 'https://flopsi69.github.io/crs/crello/tips';
 
 let stylesList = `
+  .lav-trigger {
+    background: #2053C9;
+    border-radius: 8px;
+    padding: 15px;
+    width: 200px;
+  }
+  .lav-trigger__head {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 14px;
+  }
+  .lav-trigger__label {
+    background: #F5EEE7;
+    border-radius: 5px;
+    font-weight: bold;
+    font-size: 12px;
+    line-height: 14px;
+    color: #2053C9;
+    padding: 2px 8px;
+    margin-right: 9px;
+    font-family: ProximaBold;
+  }
+  .lav-trigger__placehoder {
+    
+  }
+  .lav-trigger
+  .lav-trigger
+  .lav-trigger
   .lav-tip {
     display: flex;
     justify-content: space-between;
@@ -129,6 +158,10 @@ let stylesList = `
     border-radius: 8px;
     padding: 18px 35px;
   }
+
+  .modal__body iframe {
+    width: 100%;
+  }
   
   .modal__close {
     position: absolute;
@@ -181,6 +214,42 @@ function bannerInit() {
     .insertAdjacentHTML('afterend', bannerEl);
 }
 bannerInit();
+
+function triggerInit() {
+  let triggerIncrement = 0;
+  let triggerEl = `
+    <div class='lav-trigger'>
+      <div class='lav-trigger__head'>
+        <div class='lav-trigger__label'>Tip</div>
+        <div class='lav-trigger__close'>
+          <a width="9" height="9" viewBox="0 0 9 9" fill="#ffffff" xmlns="http://www.w3.org/2000/svg">
+            <path d="M1 1L17 17" stroke="white"/>
+            <path d="M1 17L17 1" stroke="white"/>
+          </a>
+        </div>
+      </div>
+
+      <div class='lav-trigger__placeholder'>
+        <img src='${REPO_DIR}/trigger-placeholder.png'>
+      </div>
+
+      <div class='lav-trigger__caption'>Learn how to animate your text</div>
+    </div>
+  `;
+  document
+    .querySelectorAll("#sidebar button[data-categ='sidebar']")
+    .forEach(function (btn) {
+      btn.addEventListener('click', function () {
+        btn++;
+        console.log('clicked btn:', btn);
+        if (btn > 3) {
+          activateBanner();
+        }
+      });
+    });
+
+  function activateBanner() {}
+}
 initModal();
 function initModal() {
   let modal = `
@@ -219,7 +288,6 @@ function initModal() {
 
   function modalClose() {
     modalEl.classList.remove('modal_active');
-    document.querySelector('.modal__body').style.display = 'none';
   }
 
   document.querySelectorAll('.modal-trigger').forEach(function (btn) {
