c4fad8fc45edc676d0223357c50b14d2fae3f3e6
Alex <flopsi69@gmail.com>
2021-06-10 23:32:31 +0300

    fix

---
+++ biggerpockets/newPost/index.js
@@ -0,0 +1,123 @@
+function gaEvent(action, label = '') {
+  window.dataLayer = window.dataLayer || [];
+
+  try {
+    let eventObj = {
+      event: 'ga_event',
+      eventCategory: 'Exp — PDP: add phone number',
+      eventAction: action
+    };
+
+    if (label) {
+      eventObj['eventLabel'] = label;
+    }
+
+    dataLayer.push(eventObj);
+  } catch (e) {}
+}
+
+const REPO_DIR = 'https://flopsi69.github.io/crs/biggerpockets/newPost';
+
+/* STYLES insert start */
+let stylesList = `
+  .lav-up {
+    position: relative;
+    display: flex;
+    max-width: 565px;
+    background: #FFFFFF;
+    box-shadow: 0px 0px 3px 1px rgba(0, 0, 0, 0.09);
+    padding: 30px;
+    margin-bottom: 45px;
+  }
+  .lav-up__close {
+    position: absolute;
+    top: 25px;
+    right: 25px;
+    width: 18px;
+    height: 18px;
+    transition: 0.2s;
+    cursor: pointer;
+    padding: 2px;
+    box-sizing: border-box;
+  }
+  .lav-up__close:hover {
+    fill: #4197ED;
+    opacity: 0.8;
+  }
+  .lav-up__photo {
+    margin-right: 30px;
+    width: 100px;
+    line-height: 0;
+    flex-shrink: 0;
+  }
+  .lav-up__photo img {
+    max-width: 100%;
+    max-height: 100%;
+  }
+  .lav-up__title {
+    margin-bottom: 20px;
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 27px;
+    color: #34414D;
+  }
+  .lav-up__descr {
+    font-size: 14px;
+    line-height: 21px;
+    color: #34414D;
+    margin-bottom: 20px;
+  }
+  .lav-up__link {
+    max-width: 205px;
+    width: 100%;
+    text-align: center;
+  }
+`;
+
+// connect to DOM
+let styles = document.createElement('style');
+styles.id = 'go-phone-styles';
+styles.innerHTML = stylesList;
+document.body.appendChild(styles);
+
+/* STYLES insert end */
+if (sessionStorage.getItem('closeExpBanner') != 'yes') {
+  initExp();
+}
+function initExp() {
+  console.log('initExp');
+  let blockEl = `
+    <div class="lav-up">
+      <svg class="lav-up__close" fill='#99A0A6' xmlns="http://www.w3.org/2000/svg" width="357" height="357" viewBox="0 0 357 357"><path d="M357 35.7L321.3 0 178.5 142.8 35.7 0 0 35.7l142.8 142.8L0 321.3 35.7 357l142.8-142.8L321.3 357l35.7-35.7-142.8-142.8z"/></svg>
+
+      <div class="lav-up__photo"></div>
+      <div class="lav-up__info">
+        <div class="lav-up__title">Upgrade to PRO</div>
+
+        <div class="lav-up__descr">Get a <strong>10X</strong> more attention, comments, and replies with <br> a PRO badge & many other features.</div>
+
+        <a class="button lav-up__link" href="https://www.biggerpockets.com/membership-types">Upgrade to PRO</a>
+      </div>
+    </div>
+  `;
+
+  document
+    .querySelector('.forums-content')
+    .insertAdjacentHTML('afterbegin', blockEl);
+
+  document
+    .querySelector('.lav-up__photo')
+    .insertAdjacentElement(
+      'afterbegin',
+      document
+        .querySelector('.site-navigation .nav-user .avatar')
+        .cloneNode(true)
+    );
+
+  document
+    .querySelector('.lav-up__close')
+    .addEventListener('click', function () {
+      sessionStorage.setItem('closeExpBanner', 'yes');
+      document.querySelector('.lav-up').remove();
+    });
+}
--- biggerpockets/signUp/index.js
+++ biggerpockets/signUp/index.js
@@ -43,10 +43,11 @@ let stylesList = `
     max-width: 250px;
     margin: 20px auto 30px;
     text-align: center;
-    font-weight: 500;
+    font-weight: 600;
     font-size: 14px;
     line-height: 19px;
     color: #000000;
+    letter-spacing: 0.3px;
   }
   .lav-list {
     font-size: 14px;
@@ -57,19 +58,68 @@ let stylesList = `
     margin-top: 15px;
   }
   .lav-list__item {
-    positon: relative;
+    position: relative;
+    padding-left: 20px;
   }
   .lav-list__item:before {
     content: '';
     position: absolute;
-    left: 0;
-    top: 0;
+    left: -15px;
+    top: 1px;
     width: 22px;
     height: 22px;
     background: url(${REPO_DIR}/img/icon-check.svg) center no-repeat;
     background-size: contain;
   }
-
+  .onboarding-header-logo {
+    text-align: center;
+    padding-top: 0;
+  }
+  .log-in-container, .sign-up-container {
+    padding: 20px;
+    width: 320px;
+  }
+  .log-in-signup-wrapper .sign-up-container .primary-prompt {
+    margin-top: 10px;
+    font-weight: 600;
+    font-size: 24px;
+    line-height: 33px;
+    color: #000000;
+  }
+  .sign-up-secondary-prompt {
+    font-weight: 600;
+    font-size: 14px;
+    line-height: 19px;
+    text-align: center;
+    letter-spacing: 0.2px;
+    color: #000000;
+    margin-bottom: 20px!important;
+  }
+  .signup-form .oauth-sign-up-container-bottom .google-log-in-button {
+    margin-bottom: 15px;
+  }
+  .log-in-container label, .sign-up-container label {
+    font-weight: 600;
+    font-size: 12px;
+    line-height: 16px;
+    color: #000000;
+  }
+  .sign-up-section-seperator .sign-up-notice {
+    font-size: 10px;
+    padding: 0 15px;
+    font-weight: 600;
+  }
+  .signup-form .sign-up-section-seperator {
+    margin-bottom: 15px;
+  }
+  .log-in-container .heading, .log-in-container p, .sign-up-container .heading, .sign-up-container p {
+    margin-bottom: 12px;
+  }
+  .log-in-container .subtext, .sign-up-container .subtext {
+    font-size: 10px;
+    line-height: 14px;
+    color: #C7C7C7;
+  }
   
 `;
 
@@ -91,24 +141,43 @@ document.body.appendChild(styles);
 //     console.log('try one more..');
 //   }
 // }, 500);
-
+initExp();
 function initExp() {
   console.log('initExp');
   addSideText();
+  moveForm();
 }
 
 function addSideText() {
   let sideEl = `
     <div class='lav-caption'>Become a part of biggest and growing real estate investors community!</div>
     <ul class='lav-list'>
-      <li class='lav-list__item'>Avoid mistakes by tapping into the collective knowledge of more than 2 million members. </li>
+      <li class='lav-list__item'>Avoid mistakes by tapping into the collective knowledge of more than <strong>2 million</strong> members. </li>
       <li class='lav-list__item'>Determine a property’s cash flow potential in minutes with interactive calculators and tools.</li>
       <li class='lav-list__item'>Access our Marketplace to find investor-friendly agents, financing options, and more.</li>
-      <li class='lav-list__item'>Plus, get a free copy of the Ultimate Beginner’s Guide to Real Estate Investing.</li>
+      <li class='lav-list__item'>Plus, <strong>get a free copy</strong> of the Ultimate Beginner’s Guide to Real Estate Investing.</li>
     <ul>
   `;
 
   document
     .querySelector('.why-create-content .heading')
     .insertAdjacentHTML('afterend', sideEl);
+}
+
+function moveForm() {
+  document
+    .querySelector('.signup-form + .oauth-sign-up-container-bottom ')
+    .insertAdjacentElement(
+      'beforeend',
+      document.querySelector(
+        '.signup-form + .oauth-sign-up-container-bottom .sign-up-section-seperator'
+      )
+    );
+
+  document
+    .querySelector('.sign-up-secondary-prompt')
+    .insertAdjacentElement(
+      'afterend',
+      document.querySelector('.signup-form + .oauth-sign-up-container-bottom')
+    );
 }
