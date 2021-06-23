bd35da397b5c1fa94b798af6ee6bc3c09ce3035c
Alex <flopsi69@gmail.com>
2021-06-23 18:07:53 +0300

    fix

--- biggerpockets/newPost/index.js
+++ biggerpockets/newPost/index.js
@@ -152,7 +152,7 @@ function initExp() {
       <svg class="lav-up__close" fill='#99A0A6' xmlns="http://www.w3.org/2000/svg" width="357" height="357" viewBox="0 0 357 357"><path d="M357 35.7L321.3 0 178.5 142.8 35.7 0 0 35.7l142.8 142.8L0 321.3 35.7 357l142.8-142.8L321.3 357l35.7-35.7-142.8-142.8z"/></svg>
 
       <div class="lav-up__photo">
-        <img src='${REPO_DIR}/photo.png'/>
+        <img class='avatar' src='${REPO_DIR}/photo.png'/>
         <img class='lav-up__photo-icon' src='${REPO_DIR}/icon-photo.svg'/>
       </div>
       <div class="lav-up__info">
---
+++ biggerpockets/signUp/index-mob.js
@@ -0,0 +1,276 @@
+function gaEvent(action, label = '') {
+  window.dataLayer = window.dataLayer || [];
+
+  try {
+    let eventObj = {
+      event: 'ga_event',
+      eventCategory: 'Exp — PDP: add phone number',
+      eventAction: action
+    };
+
+    if (label) {
+      eventObj['eventLabel'] = label;
+    }
+
+    dataLayer.push(eventObj);
+  } catch (e) {}
+}
+
+const REPO_DIR = 'https://flopsi69.github.io/crs/biggerpockets/signUp';
+
+/* STYLES insert start */
+let stylesList = `
+  .log-in-signup-wrapper .why-create-content .content-body {
+    display: none;
+  }
+  .log-in-signup-wrapper .why-create-content .content-end {
+    font-size: 24px;
+    line-height: 1.5;
+    color: #34414D;
+    font-weight: 400;
+    margin-top: 30px;
+    margin-bottom: 20px;
+  }
+
+  .log-in-signup-wrapper .why-create-container {
+    padding: 20px;
+    box-shadow: 0px 0px 3px 1px rgb(0 0 0 / 9%);
+    box-sizing: border-box;
+    width: 100%;
+  }
+
+  .log-in-signup-wrapper .why-create-container {
+    margin-left: 0;
+  }
+
+  .log-in-signup-wrapper .why-create-container:before {
+    display: none!important;
+  }
+
+  .log-in-signup-wrapper .why-create-content {
+    margin-top: 0;
+  }
+  
+  .log-in-signup-wrapper .why-create-content .heading {
+    margin-bottom: 0;
+    padding-bottom: 7px;
+  }
+  .lav-caption {
+    margin: 10px auto 25px;
+    font-weight: 300;
+    font-size: 12px;
+    line-height: 16px;
+    color: #5D6771;
+    letter-spacing: 0.3px;
+  }
+  .lav-list {
+    font-size: 12px;
+    line-height: 1.5;
+    color: #5D6771
+  }
+  .lav-list__item + .lav-list__item {
+    margin-top: 18px;
+  }
+  .lav-list__item {
+    position: relative;
+    padding-left: 40px;
+  }
+  .lav-list__item:before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 1px;
+    width: 22px;
+    height: 22px;
+    background: url(${REPO_DIR}/img/icon-check.svg) center no-repeat;
+    background-size: contain;
+  }
+  .log-in-signup-wrapper {
+    padding: 0;
+  }
+  .log-in-signup-wrapper .modal {
+    padding: 0;
+    max-width: 100%;
+    min-height: 100vh;
+  }
+  .log-in-signup-wrapper .sign-up-container {
+    background: #FFFFFF;
+    padding: 30px 20px 20px;
+    box-shadow: 0px 0px 3px 1px rgb(0 0 0 / 9%)!important;
+  }
+  .log-in-signup-wrapper .log-in-signup-wrapper .log-in-sign-up-container .log-in-sign-up-inner {
+    max-width: 880px;
+  }
+  .log-in-signup-wrapper .sign-up-container {
+    width: 100%;
+    margin-bottom: 30px;
+  }
+  .log-in-signup-wrapper .onboarding-header-logo {
+    padding-top: 0;
+    margin-bottom: 20px;
+  }
+  .log-in-signup-wrapper .onboarding-header-logo a {
+    display: inline-block;
+  }
+  .log-in-signup-wrapper .onboarding-logo-link>object {
+    height: 25px;
+    width: auto;
+  }
+  .log-in-signup-wrapper .log-in-signup-wrapper .sign-up-container .primary-prompt {
+    margin-top: 0;
+    text-align: left;
+    display: flex;
+    justify-content: space-between;
+  }
+
+  .log-in-signup-wrapper.modal-container .modal header .close {
+    width: 24px;
+    height: 24px;
+    right: 50px;
+    top: 50px;
+    font-size: 22px;
+  }
+  .log-in-signup-wrapper .sign-up-secondary-prompt {
+    text-align: left;
+    color: #5D6771;
+    font-weight: 300;
+    font-size: 12px;
+    line-height: 16px;
+    margin-bottom: 25px;
+  }
+  .log-in-signup-wrapper .sign-up-container  .oauth-sign-up-container-bottom .google-log-in-button {
+    margin-bottom: 20px;
+  }
+  .sign-up-container .sign-up-section-seperator .sign-up-notice {
+    text-transform: lowercase;
+    padding: 0 15px;
+  }
+
+  .sign-up-container label {
+    font-size: 14px;
+    line-height: 21px;
+    color: #5D6771;
+    font-weight: 400;
+    margin-bottom: 5px;
+  }
+  .sign-up-container p {
+    margin-bottom: 15px;
+  }
+  .sign-up-container p.button-container {
+    margin-bottom: 0;
+  }
+  .sign-up-container .subtext {
+    font-weight: 300;
+    font-size: 12px;
+    line-height: 16px;
+    color: #99A0A6;
+  }
+  .sign-up-container .sign-up-section-seperator {
+    margin-bottom: 15px;
+  }
+  .log-in-signup-wrapper .signup-form #social_user_full_name + .subtext {
+    display: none;
+  }
+  .lav-label-subtext {
+    font-weight: 300;
+    font-size: 12px;
+    line-height: 16px;
+    color: #99A0A6;
+    padding-left: 10px;
+  }
+
+  .log-in-signup-wrapper .log-in-sign-up-container.sign-up-active .log-in-container, .log-in-signup-wrapper .log-in-sign-up-container.sign-up-active .sign-up-container {
+    box-sizing: border-box;
+  }
+  .why-create-container {
+    display: block
+  }
+  .log-in-signup-wrapper .sign-up-container {
+    float: none;
+  }
+`;
+
+// connect to DOM
+let styles = document.createElement('style');
+styles.id = 'go-phone-styles';
+styles.innerHTML = stylesList;
+document.body.appendChild(styles);
+
+/* STYLES insert end */
+
+// CODE START ***
+// var intervalInit = setInterval(() => {
+//   if (typeof advertId !== 'undefined') {
+//     console.log('success..');
+//     clearInterval(intervalInit);
+//     init();
+//   } else {
+//     console.log('try one more..');
+//   }
+// }, 500);
+initExp();
+function initExp() {
+  console.log('initExp');
+  addSideText();
+  moveForm();
+}
+
+function addSideText() {
+  let sideEl = `
+    <div class='lav-caption'>Become a part of biggest and growing real estate investors community!</div>
+    <ul class='lav-list'>
+      <li class='lav-list__item'>Avoid mistakes by tapping into the collective knowledge of more than <br> <strong>2 million</strong> members. </li>
+      <li class='lav-list__item'>Determine a property’s cash flow potential in minutes with interactive calculators and tools.</li>
+      <li class='lav-list__item'>Access our Marketplace to find investor-friendly agents, financing options, and more.</li>
+      <li class='lav-list__item'>Plus, <strong>get a free copy</strong> of the Ultimate Beginner’s Guide to Real Estate Investing.</li>
+    </ul>
+  `;
+
+  document
+    .querySelector('.log-in-signup-wrapper .why-create-content .heading')
+    .insertAdjacentHTML('afterend', sideEl);
+}
+
+function moveForm() {
+  document
+    .querySelector(
+      '.log-in-signup-wrapper .signup-form + .oauth-sign-up-container-bottom '
+    )
+    .insertAdjacentElement(
+      'beforeend',
+      document.querySelector(
+        '.log-in-signup-wrapper .signup-form + .oauth-sign-up-container-bottom .sign-up-section-seperator'
+      )
+    );
+
+  document
+    .querySelector('.log-in-signup-wrapper .sign-up-secondary-prompt')
+    .insertAdjacentElement(
+      'afterend',
+      document.querySelector(
+        '.log-in-signup-wrapper .signup-form + .oauth-sign-up-container-bottom'
+      )
+    );
+
+  document
+    .querySelector(".log-in-signup-wrapper [for='social_user_full_name']")
+    .insertAdjacentHTML(
+      'beforeend',
+      "<span class='lav-label-subtext'>Use your real name</span>"
+    );
+
+  document
+    .querySelector(".log-in-signup-wrapper [for='social_user_password']")
+    .insertAdjacentHTML(
+      'beforeend',
+      "<span class='lav-label-subtext'>Use at least 8 characters.</span>"
+    );
+
+  document.querySelector(
+    '.log-in-signup-wrapper .signup-form #social_user_password + .subtext'
+  ).innerHTML = document
+    .querySelector(
+      '.log-in-signup-wrapper .signup-form #social_user_password + .subtext'
+    )
+    .innerHTML.replace('Use at least 8 characters. ', '');
+}
