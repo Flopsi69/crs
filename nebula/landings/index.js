59e1c68d94e0e033e266ba13f43dcc7b51fef1b0
Alex <flopsi69@gmail.com>
2021-08-20 15:28:13 +0300

    fix

---
+++ nebula/landings/index.js
@@ -0,0 +1,64 @@
+(function () {
+  console.log('initExp');
+  function gaEvent(action, label, value) {
+    if (!action) {
+      action = '';
+    }
+    if (!label) {
+      label = '';
+    }
+    if (!value) {
+      value = '';
+    }
+    window.dataLayer = window.dataLayer || [];
+    try {
+      let eventObj = {
+        event: 'event-to-ga',
+        eventCategory: 'Exp - Checkout_more_info',
+        eventAction: action,
+        eventLabel: label,
+        eventValue: value,
+      };
+      dataLayer.push(eventObj);
+      console.log('FireEvent', eventObj);
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  /* STYLES insert start */
+  const REPO_DIR = 'https://flopsi69.github.io/crs/nebula/landings';
+
+  let stylesList = ``;
+
+  // let stylesEl = document.createElement('style');
+  // stylesEl.id = 'lav-styles';
+  // stylesEl.innerHTML = stylesList;
+  // document.body.appendChild(stylesEl);
+
+  // let observer = new MutationObserver(mutations => {
+  //   for (let mutation of mutations) {
+  //     for (let node of mutation.addedNodes) {
+  //       if (!(node instanceof HTMLElement)) continue;
+  //       if (node.classList.contains('cart-page')) {
+  //         initExp();
+  //         observer.disconnect();
+  //       }
+  //     }
+  //   }
+  // });
+
+  // observer.observe(document.querySelector('body'), {
+  //   childList: true,
+  //   subtree: true
+  // });
+  /* STYLES insert end */
+
+  function initExp() {
+    console.log('Exp init!');
+    document.querySelectorAll('.order_main_wrap').forEach(function (orderEl) {
+      initOrderStyle(orderEl);
+    });
+    initModal();
+  }
+})();
