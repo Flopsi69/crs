9c6ac4b52187d5d69ea6a5124e617bba8a2af2e1
Alex <flopsi69@gmail.com>
2021-08-31 15:10:15 +0300

    fix

--- nebula/landings/index.js
+++ nebula/landings/index.js
@@ -1,4 +1,19 @@
 (function () {
+  (function (h, o, t, j, a, r) {
+    h.hj =
+      h.hj ||
+      function () {
+        (h.hj.q = h.hj.q || []).push(arguments);
+      };
+    h._hjSettings = { hjid: 1209457, hjsv: 6 };
+    a = o.getElementsByTagName('head')[0];
+    r = o.createElement('script');
+    r.async = 1;
+    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+  hj('trigger', 'hp_9_cta');
+
   console.log('initExp');
   function gaEvent(action, label, value) {
     if (!action) {
@@ -7,17 +22,13 @@
     if (!label) {
       label = '';
     }
-    if (!value) {
-      value = '';
-    }
     window.dataLayer = window.dataLayer || [];
     try {
       let eventObj = {
         event: 'event-to-ga',
-        eventCategory: 'Exp - Checkout_more_info',
+        eventCategory: 'Exp - HP with 9 CTAs',
         eventAction: action,
         eventLabel: label,
-        eventValue: value,
       };
       dataLayer.push(eventObj);
       console.log('FireEvent', eventObj);
@@ -39,6 +50,28 @@
   stylesEl.id = 'lav-styles';
   stylesEl.innerHTML = stylesList;
   document.body.appendChild(stylesEl);
+
+  gaEvent('loaded');
+
+  document.addEventListener('click', function (e) {
+    if (
+      e.target.classList.contains('learn__item') ||
+      e.target.closest('.learn__item')
+    ) {
+      if (e.target.classList.contains('learn__item')) {
+        gaEvent(
+          'click on Why subscribe',
+          e.target.querySelector('.learn__item-caption').innerText
+        );
+      } else {
+        gaEvent(
+          'click on Why subscribe',
+          e.target.closest('.learn__item').querySelector('.learn__item-caption')
+            .innerText
+        );
+      }
+    }
+  });
 
   // let observer = new MutationObserver(mutations => {
   //   for (let mutation of mutations) {
