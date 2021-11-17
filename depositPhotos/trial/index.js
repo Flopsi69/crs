835aa5f84b5385ed5e8734096db827ae177f49be
Alexander Lytovchenko <flopsi69@gmail.com>
2021-11-17 17:19:40 +0200

    fix class

--- depositPhotos/trial/index.js
+++ depositPhotos/trial/index.js
@@ -403,7 +403,7 @@
       for (let node of mutation.addedNodes) {
         if (!(node instanceof HTMLElement)) continue;
 
-        if (node.classList.contains('.wrapper')) {
+        if (node.classList.contains('wrapper')) {
           initExp();
         }
         console.log(node);
