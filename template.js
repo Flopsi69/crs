4b1a635ca5ce1031dc6ad184be239a19ec40ea3a
Alexander Lytovchenko <flopsi69@gmail.com>
2022-03-08 18:08:39 +0200

    create template for tests

---
+++ template.js
@@ -0,0 +1,84 @@
+console.log('initExp');
+
+/********* Settings **********/
+const settings = {
+  dir: 'https://flopsi69.github.io/crs/depositPhotos/mayAlsoLike',
+  hj: false,
+  observe: false,
+};
+
+//Hotjar
+if (settings.hj) {
+  try {
+    (function (h, o, t, j, a, r) {
+      h.hj =
+        h.hj ||
+        function () {
+          (h.hj.q = h.hj.q || []).push(arguments);
+        };
+      h._hjSettings = { hjid: 410340, hjsv: 6 };
+      a = o.getElementsByTagName('head')[0];
+      r = o.createElement('script');
+      r.async = 1;
+      r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+      a.appendChild(r);
+    })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+    window.hj =
+      window.hj ||
+      function () {
+        (hj.q = hj.q || []).push(arguments);
+      };
+    hj('trigger', 'also_like');
+  } catch (e) {}
+}
+
+// Alalytic
+function gaEvent(action, label) {
+  if (!label) {
+    label = '';
+  }
+  try {
+    var objData = {
+      event: 'gaEv',
+      eventCategory: 'Experiment — also like',
+      eventAction: action,
+      eventLabel: label,
+      eventValue: '',
+    };
+    console.log('EventFire:', objData);
+    dataLayer.push(objData);
+  } catch (e) {}
+}
+
+// Observe
+if (settings.observe) {
+  let observer = new MutationObserver((mutations) => {
+    for (let mutation of mutations) {
+      for (let node of mutation.addedNodes) {
+        if (!(node instanceof HTMLElement)) continue;
+
+        // Code Here
+      }
+    }
+  });
+
+  let demoElem = document.body;
+
+  observer.observe(demoElem, { childList: true, subtree: true });
+}
+
+// Styles
+const styles = `
+  
+`;
+
+const stylesEl = document.createElement('style');
+stylesEl.innerHTML = styles;
+document.body.appendChild(stylesEl);
+/*** STYLES / end ***/
+
+/********* Custom Code **********/
+init();
+function init() {
+  console.log('init');
+}
