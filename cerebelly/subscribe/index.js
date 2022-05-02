9b20f90940915570dcc8fcf8fb0eaf3d976da393
Alexander Lytovchenko <flopsi69@gmail.com>
2022-05-02 17:04:28 +0300

    fix

--- cerebelly/subscribe/index.js
+++ cerebelly/subscribe/index.js
@@ -69,34 +69,177 @@ if (settings.observe) {
 
 // Styles
 const styles = `
-  .css-1gs8fg8 .summary-table .sum-row.total.coupon .caption,
   .css-1gs8fg8 .summary-table .sum-row.total.coupon .total, 
-  .css-x4jdip .box .wrapper .total {
-    color: #FC4D38;
+  .css-x4jdip .box .wrapper .total, .box .wrapper .lines .content .sum-row .total.with-discount span:last-child, .css-xf71d4 .text-green {
+    color: #FC4D38!important;
   }
 
-  .css-xf71d4 .text-green {
-    color: #FC4D38 !important;
+  .subscribe-all {
+    display: none!important;
   }
 
-  .subscribe-all *:not(.test),
-  .action .subscribe {
-    display: none !important;
-  }
+  // .subscribe-all *:not(.test),
+  // .action .subscribe {
+  //   display: none !important;
+  // }
 
-  p.test {
-    margin-bottom: 0;
-    text-decoration: underline;
+  .lav-caption {
     color: #3856A7;
     font-size: 12px;
-  }
-
-  .tm-text {
-    color: #3856A7 !important;
-    font-size: 12px !important;
     font-weight: 700;
-    text-decoration: underline;
+    line-height: 1;
+    border-bottom: 1px solid
   }
+  .box .wrapper .info h3 {
+    font-weight: 700!important;
+    line-height: 1.2!important;
+  }
+  .remove-box.mobile {
+    margin-right: 0!important;
+  }
+  .remove-box.mobile img {
+    border: 1px solid #3856A7;
+    border-radius: 50%;
+    padding: 2px;
+    width: 20px;
+  }
+  .box .wrapper .total {
+    font-size: 14px!important;
+  }
+  .e-page-content-wrap .box .image img {
+    width: 121px!important;
+  }
+  .e-page-content-wrap .box .image {
+    margin-right: 8px!important;
+  }
+  .e-page-content-wrap .box {
+    padding: 0!important;
+    margin: 32px 0 15px!important;
+  }
+  .e-page-content-wrap .box .wrapper .info {
+    margin-bottom: 8px!important;
+  }
+  .box .wrapper .lines .toggle .items-count {
+    font-size: 12px;
+    line-height: 12px;
+  }
+  .box .wrapper .lines .toggle .items-count img {
+    width: 9px;
+  }
+  .box .wrapper .subtotal {
+    font-size: 14px!important;
+  }
+  .tm-text {
+    display: none;
+  }
+  .lav-caption__cancel-wrap {
+    padding: 0 15px;
+    text-align: center;
+    width: 100%;
+  }
+  .lav-caption__cancel-wrap div {
+    display: inline;
+  }
+  .css-tucdor .summary-table {
+    padding-top: 42px;
+  }
+  .css-tucdor .summary-table .checkout-title {
+    margin-bottom: 12px;
+    font-size: 14px;
+    letter-spacing: 0.15em;
+  }
+  .summary-table .sum-row .caption, .summary-table .sum-row .total {
+    font-size: 14px!important;
+  }
+  .summary-table .sum-row.total + .summary-table .sum-row.total {
+    margin-top: 14px!important;
+    margin-bottom: 0!important;
+  }
+  .summary-table .sum-row.promo-code {
+    margin: 26px 0;
+  }
+  .summary-table .sum-row.promo-code p {
+    font-size: 12px!important;
+    font-weight: 700!important;
+  }
+  .summary-table .sum-row.order-total .caption {
+    font-size: 16px!important;
+    font-weight: 700!important;
+  }
+  .summary-table .sum-row.order-total .total {
+    font-weight: 700!important;
+    font-size: 16px!important;
+  }
+  .summary-table .sum-row.order-total .total .total {
+    font-size: 13px!important;
+  }
+  .summary-table .sum-row .caption, .summary-table .sum-row .total {
+    font-weight: 700!important;
+  }
+`;
+
+const modalEl = `
+  <style>
+    .lav-modal {
+      display: flex;
+      position: fixed;
+      z-index: 999999;
+      left: 0;
+      top: 0;
+      right: 0;
+      bottom: 0;
+      background: rgba(206, 211, 242, 0.94);
+      margin-top: auto;
+    }
+    .lav-modal__inner {
+      background: #E3EAFD;
+      border-radius: 10px 10px 0 0;
+      margin-top: auto;
+      padding: 30px 25px 17px;
+    }
+    .lav-modal__title {
+      font-weight: 700;
+      font-size: 28px;
+      line-height: 34px;
+      color: #3856A7;
+      span {
+        color: #FC4D38;
+      }
+    }
+  </style>
+  <div class='lav-confirm lav-modal'>
+    <div class='lav-confirm__inner lav-modal__inner'>
+      <div class='lav-confirm__title lav-modal__title'>A flexible subscription adjusted <span>to your needs</span></div>
+      <div class='lav-confirm__image'>
+        <img src='${settings.dir}'>
+      </div>
+      <ul class='lav-confirm__list lav-modal__list'>
+        <li class='lav-confirm__item'><strong>15% OFF</strong> our science-backed baby food.</li>
+        <li class='lav-confirm__item'>Easily customize each order exactly how you'd like.</li>
+        <li class='lav-confirm__item'>Never run out of baby food with flexible shipping options.</li>
+        <li class='lav-confirm__item'>Complete control over confirmlation or rescheduling. </li>
+      </ul>
+      <div class='lav-confirm__note'>
+        Subscribers can get 2<strong>8 more days</strong> of our baby food <strong>for FREE</strong> each year by taking advantage of the 15% discounted
+      </div>
+      <div class='lav-confirm__caption-wrap'>
+        <div class='lav-confirm__caption lav-caption'>Skip offer and choose one-time purchase</div>
+      </div>
+      <button class="button primary red lav-confirm__btn lav-modal__btn">Continue</button>
+    </div>
+
+    <div class='lav-cancel__inner lav-modal__inner' style='display: none;'>
+      <div class='lav-cancel__title lav-modal__title'>A flexible subscription adjusted <span>to your needs</span></div>
+      <div class='lav-cancel__image'>
+        <img src=''>
+      </div>
+      <ul class='lav-cancel__list lav-modal__list'>
+        <li class='lav-cancel__item'>Need to reschedule a shipment? Just click 'My subscriptions' and select the next shipment date.</li>
+        <li class='lav-cancel__item'>No longer interested in Clean Label Certified food? Leave us by heading to 'Account settings' and clicking on 'Cancel'.</li>
+      </ul>
+      <button class="button primary red lav-cancel__btn lav-modal__btn">Got it</button>
+    </div>
+  </div>
 `;
 
 const stylesEl = document.createElement('style');
@@ -108,15 +251,37 @@ document.body.appendChild(stylesEl);
 init();
 function init() {
   console.log('init');
+  if (
+    !document.querySelector(
+      '.subscribe-all .container-checkbox .hidden-checkbox'
+    ).checked
+  ) {
+    document.querySelector('.subscribe-all .container-checkbox').click();
+  }
+
+  initModal();
+
   document
     .querySelector('.subscribe-all')
     .insertAdjacentHTML(
-      'afterbegin',
-      `<p class="test">Why subscribing is better?</p>`
+      'beforebegin',
+      `<div class="lav-caption lav-caption__head">Why subscribing is better?</div>`
     );
-  document.querySelector('.tm-text').innerText = 'How to cancel subscription?';
 
-  let inputEvent = new Event('input');
+  document.querySelector('.custom-column').insertAdjacentHTML(
+    'beforebegin',
+    `
+      <div class='lav-caption__cancel-wrap'>
+        <div class="lav-caption lav-caption__cancel">How to cancel subscription?</div>
+      </div>
+      `
+  );
 
-  document.querySelector('.select__input').dispatchEvent(inputEvent);
+  // let inputEvent = new Event('input');
+
+  // document.querySelector('.select__input').dispatchEvent(inputEvent);
+}
+
+function initModal() {
+  document.querySelector('body').insertAdjacentHTML('beforeend', modalEl);
 }
