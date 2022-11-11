67c230cbdd624b7fd6e678bdce0b36fc14534500
Alexander Lytovchenko <flopsi69@gmail.com>
2022-11-11 14:26:34 +0200

    fix

--- comparamais/editUser/index.js
+++ comparamais/editUser/index.js
@@ -1004,7 +1004,6 @@ function changeCardView() {
       card.querySelector('.card__apply a').innerText = 'Como fazer o pedido';
 
       setTimeout(() => {
-        console.lot('testik');
         card
           .querySelector('.toggle--on')
           .addEventListener('click', function () {
