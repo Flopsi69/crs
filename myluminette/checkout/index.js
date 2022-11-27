2ca18f6e9b55c4090174217310db154976a3495e
Alexander Lytovchenko <flopsi69@gmail.com>
2022-11-28 00:32:22 +0200

    fix

--- myluminette/checkout/index.js
+++ myluminette/checkout/index.js
@@ -4624,56 +4624,56 @@ function initPdp() {
       },
     }).mount();
 
-    $(() => {
-      const $slider = $('.slider');
-      // $showMore = $section.find('.show-more');
+    // JS Slider
+    const $slider = $('.slider');
+    // $showMore = $section.find('.show-more');
 
-      let sliderActivated = false;
-      let showMoreActivated = false;
-      const options = {
-        centerMode: true,
-        centerPadding: 0,
-        infinite: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        speed: 375,
-        dots: true,
-      };
+    let sliderActivated = false;
+    let showMoreActivated = false;
+    const options = {
+      centerMode: true,
+      centerPadding: 0,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      speed: 375,
+      dots: true,
+    };
 
-      // const showMoreHandler = () => {
-      //   $('.customer-reviews-page .slide:not(:visible):lt(3)').fadeIn(() => {
-      //     if ($('.customer-reviews-page .slide:not(:visible)').length === 0) {
-      //       $showMore.addClass('d-none');
-      //     }
-      //   });
-      // };
+    // const showMoreHandler = () => {
+    //   $('.customer-reviews-page .slide:not(:visible):lt(3)').fadeIn(() => {
+    //     if ($('.customer-reviews-page .slide:not(:visible)').length === 0) {
+    //       $showMore.addClass('d-none');
+    //     }
+    //   });
+    // };
 
-      const showMoreInit = () => {
-        $('.customer-reviews-page .slide:gt(2)').hide();
-        $showMore.removeClass('d-none');
+    const showMoreInit = () => {
+      $('.customer-reviews-page .slide:gt(2)').hide();
+      $showMore.removeClass('d-none');
 
-        if (!showMoreActivated) {
-          $showMore.click(showMoreHandler);
-          showMoreActivated = true;
+      if (!showMoreActivated) {
+        $showMore.click(showMoreHandler);
+        showMoreActivated = true;
+      }
+    };
+
+    const resizeHandler = () => {
+      const isMobile = window.matchMedia('(max-width: 991.98px)').matches;
+      if (isMobile) {
+        if (sliderActivated) {
+          $slider.slick('unslick');
+          sliderActivated = false;
         }
-      };
+        // showMoreInit();
+      } else if (!sliderActivated) {
+        try {
+          const classificateDots = (prevDotIndex) => {
+            const prevPrevDotIndex = prevDotIndex - 1;
+            const nextDotIndex = prevDotIndex + 2;
+            const nextNextDotIndex = prevDotIndex + 3;
 
-      const resizeHandler = () => {
-        const isMobile = window.matchMedia('(max-width: 991.98px)').matches;
-        if (isMobile) {
-          if (sliderActivated) {
-            $slider.slick('unslick');
-            sliderActivated = false;
-          }
-          // showMoreInit();
-        } else if (!sliderActivated) {
-          try {
-            const classificateDots = (prevDotIndex) => {
-              const prevPrevDotIndex = prevDotIndex - 1;
-              const nextDotIndex = prevDotIndex + 2;
-              const nextNextDotIndex = prevDotIndex + 3;
-
-              const $dots = $slider.find('.slick-dots > li').removeClass(`
+            const $dots = $slider.find('.slick-dots > li').removeClass(`
                   slick-prev 
                   slick-next 
                   slick-prev-prev 
@@ -4682,56 +4682,55 @@ function initPdp() {
                   slick-next-max
                 `);
 
-              for (let i = 0; i < prevPrevDotIndex; i++) {
-                $slider.find(`li:nth-child(${i})`).addClass('slick-prev-max');
-              }
+            for (let i = 0; i < prevPrevDotIndex; i++) {
+              $slider.find(`li:nth-child(${i})`).addClass('slick-prev-max');
+            }
 
-              for (let i = $dots.length; i > nextNextDotIndex; i--) {
-                $slider.find(`li:nth-child(${i})`).addClass('slick-next-max');
-              }
+            for (let i = $dots.length; i > nextNextDotIndex; i--) {
+              $slider.find(`li:nth-child(${i})`).addClass('slick-next-max');
+            }
 
-              $slider
-                .find(
-                  `li:nth-child(${
-                    prevDotIndex > 0 ? prevDotIndex : $dots.length
-                  })`
-                )
-                .addClass('slick-prev');
+            $slider
+              .find(
+                `li:nth-child(${
+                  prevDotIndex > 0 ? prevDotIndex : $dots.length
+                })`
+              )
+              .addClass('slick-prev');
 
-              $slider
-                .find(`li:nth-child(${prevPrevDotIndex ?? 0}`)
-                .addClass('slick-prev-prev');
+            $slider
+              .find(`li:nth-child(${prevPrevDotIndex ?? 0}`)
+              .addClass('slick-prev-prev');
 
-              $slider
-                .find(
-                  `li:nth-child(${
-                    nextDotIndex > $dots.length ? 1 : nextDotIndex
-                  })`
-                )
-                .addClass('slick-next');
+            $slider
+              .find(
+                `li:nth-child(${
+                  nextDotIndex > $dots.length ? 1 : nextDotIndex
+                })`
+              )
+              .addClass('slick-next');
 
-              $slider
-                .find(`li:nth-child(${nextNextDotIndex ?? 0})`)
-                .addClass('slick-next-next');
-            };
+            $slider
+              .find(`li:nth-child(${nextNextDotIndex ?? 0})`)
+              .addClass('slick-next-next');
+          };
 
-            $slider.slick(options);
-            sliderActivated = true;
+          $slider.slick(options);
+          sliderActivated = true;
 
-            classificateDots(0);
+          classificateDots(0);
 
-            $slider.on('beforeChange', (_e, _slick, _curr, next) =>
-              classificateDots(next)
-            );
-          } catch (error) {
-            console.log(error);
-          }
+          $slider.on('beforeChange', (_e, _slick, _curr, next) =>
+            classificateDots(next)
+          );
+        } catch (error) {
+          console.log(error);
         }
-      };
+      }
+    };
 
-      $(window).resize(resizeHandler);
-      resizeHandler();
-    });
+    $(window).resize(resizeHandler);
+    resizeHandler();
   }
 
   function initModals() {
