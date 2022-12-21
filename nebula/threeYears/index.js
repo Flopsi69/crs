7489cdf3f31ca9041af05eb7da36041f5aaec8e3
Alexander Lytovchenko <flopsi69@gmail.com>
2022-12-21 14:35:08 +0200

    fix

---
+++ nebula/threeYears/index.js
@@ -0,0 +1,88 @@
+console.log('initExp');
+
+/********* Settings **********/
+const settings = {
+  dir: 'https://flopsi69.github.io/crs/depositPhotos/mayAlsoLike',
+  clarity: false,
+  observe: false,
+};
+
+//Hotjar
+if (settings.clarity) {
+  const clarityInterval = setInterval(function () {
+    if (typeof clarity == 'function') {
+      clearInterval(clarityInterval);
+      clarity('set', '', 'variant_1');
+    }
+  }, 1000);
+}
+
+// Alalytic
+function gaEvent(action, label) {
+  if (!label) {
+    label = '';
+  }
+  try {
+    var objData = {
+      event: 'gaEv',
+      eventCategory: 'Experiment — also like',
+      eventAction: action,
+      eventLabel: label,
+      eventValue: '',
+    };
+    console.log('EventFire:', objData);
+    dataLayer.push(objData);
+  } catch (e) {
+    console.log('Event Error:', e);
+  }
+}
+
+// Alalytic
+function gaEvent(name = '', desc = '', type = '', loc = '') {
+  try {
+    var objData = {
+      event: 'event-to-ga4',
+      event_name: name,
+      event_desc: desc,
+      event_type: type,
+      event_loc: loc,
+    };
+    console.dir('eventFire', objData.eventAction);
+    dataLayer.push(objData);
+  } catch (e) {
+    console.log('Event Error:', e);
+  }
+}
+
+// Observe
+if (settings.observe) {
+  let observer = new MutationObserver((mutations) => {
+    for (let mutation of mutations) {
+      for (let node of mutation.addedNodes) {
+        if (!(node instanceof HTMLElement)) continue;
+
+        // Code Here
+      }
+    }
+  });
+
+  let demoElem = document.body;
+
+  observer.observe(demoElem, { childList: true, subtree: true });
+}
+
+// Styles
+const styles = `
+  
+`;
+
+const stylesEl = document.createElement('style');
+stylesEl.innerHTML = styles;
+document.body.appendChild(stylesEl);
+/*** STYLES / end ***/
+
+/********* Custom Code **********/
+init();
+function init() {
+  console.log('init');
+}
