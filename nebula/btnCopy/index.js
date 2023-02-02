3f61caecd6c1b69c5ed0f611e69034846aa5b3d4
Alexander Lytovchenko <flopsi69@gmail.com>
2023-02-02 12:32:55 +0200

    fix

---
+++ nebula/btnCopy/index.js
@@ -0,0 +1,76 @@
+console.log('initExp');
+
+/********* Settings **********/
+const settings = {
+  dir: 'https://flopsi69.github.io/crs/nebula/btnCopy',
+  clarity: false,
+  observe: false,
+};
+
+//Hotjar
+if (settings.clarity) {
+  const clarityInterval = setInterval(function () {
+    if (typeof clarity == 'function') {
+      clearInterval(clarityInterval);
+      clarity('set', '', 'variant_1');
+    }
+  }, 1000);
+}
+
+// Alalytic
+function gaEvent(action, label) {
+  if (!label) {
+    label = '';
+  }
+  try {
+    var objData = {
+      event: 'gaEv',
+      eventCategory: 'Experiment — HP button text',
+      eventAction: action,
+      eventLabel: label,
+      eventValue: '',
+    };
+    console.log('EventFire:', objData);
+    dataLayer.push(objData);
+  } catch (e) {
+    console.log('Event Error:', e);
+  }
+}
+
+// Observe
+if (settings.observe) {
+  let observer = new MutationObserver((mutations) => {
+    for (let mutation of mutations) {
+      for (let node of mutation.addedNodes) {
+        if (!(node instanceof HTMLElement)) continue;
+
+        // Code Here
+      }
+    }
+  });
+
+  let demoElem = document.body;
+
+  observer.observe(demoElem, { childList: true, subtree: true });
+}
+
+// Styles
+const styles = `
+  
+`;
+
+const stylesEl = document.createElement('style');
+stylesEl.innerHTML = styles;
+document.body.appendChild(stylesEl);
+/*** STYLES / end ***/
+
+/********* Custom Code **********/
+init();
+function init() {
+  console.log('init');
+  document.querySelector('.jumb__btn').innerText = 'Shop DNA Tests';
+
+  document.querySelector('.jumb__btn').addEventListener('click', function () {
+    gaEvent('Click button');
+  });
+}
