86566b1b88c98690d05cc155f4611c1ed9108df7
Alexander Lytovchenko <flopsi69@gmail.com>
2023-02-21 16:58:47 +0200

    fix

--- nebula/improvePayment/index.js
+++ nebula/improvePayment/index.js
@@ -56,6 +56,115 @@ if (settings.observe) {
 
 // Styles
 const styles = `
+  .go-to-cart {
+    display: none!important;
+  }
+  .lav-preloader #__nuxt:before{
+    position: fixed;
+    left: 0;
+    content: '';
+    right: 0;
+    bottom: 0;
+    top: 0;
+    z-index: 999999999999;
+    background-color: rgba(0,0,0,.5);
+  }
+  .lds-roller {
+    display: inline-block;
+    position: relative;
+    width: 80px;
+    height: 80px;
+  }
+  .lds-roller div {
+    animation: lds-roller 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    transform-origin: 40px 40px;
+  }
+  .lds-roller div:after {
+    content: " ";
+    display: block;
+    position: absolute;
+    width: 7px;
+    height: 7px;
+    border-radius: 50%;
+    background: #fff;
+    margin: -4px 0 0 -4px;
+  }
+  .lds-roller div:nth-child(1) {
+    animation-delay: -0.036s;
+  }
+  .lds-roller div:nth-child(1):after {
+    top: 63px;
+    left: 63px;
+  }
+  .lds-roller div:nth-child(2) {
+    animation-delay: -0.072s;
+  }
+  .lds-roller div:nth-child(2):after {
+    top: 68px;
+    left: 56px;
+  }
+  .lds-roller div:nth-child(3) {
+    animation-delay: -0.108s;
+  }
+  .lds-roller div:nth-child(3):after {
+    top: 71px;
+    left: 48px;
+  }
+  .lds-roller div:nth-child(4) {
+    animation-delay: -0.144s;
+  }
+  .lds-roller div:nth-child(4):after {
+    top: 72px;
+    left: 40px;
+  }
+  .lds-roller div:nth-child(5) {
+    animation-delay: -0.18s;
+  }
+  .lds-roller div:nth-child(5):after {
+    top: 71px;
+    left: 32px;
+  }
+  .lds-roller div:nth-child(6) {
+    animation-delay: -0.216s;
+  }
+  .lds-roller div:nth-child(6):after {
+    top: 68px;
+    left: 24px;
+  }
+  .lds-roller div:nth-child(7) {
+    animation-delay: -0.252s;
+  }
+  .lds-roller div:nth-child(7):after {
+    top: 63px;
+    left: 17px;
+  }
+  .lds-roller div:nth-child(8) {
+    animation-delay: -0.288s;
+  }
+  .lds-roller div:nth-child(8):after {
+    top: 56px;
+    left: 12px;
+  }
+  @keyframes lds-roller {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }  
+  .lav-prel {
+    display: none;
+  }
+  .lav-preloader .lav-prel {
+    display: block;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    z-index: 999999999999999999999;
+    transform: translate(-50%, -50%);
+
+  }
   .pay-btn {
     min-height: 45px;
     font-weight: 700;
@@ -177,6 +286,7 @@ function initHP() {
       }
 
       document.querySelector('.shoppingCart').click();
+      addPreloader();
     }
   });
 
@@ -281,4 +391,13 @@ function initCart() {
   if (document.querySelector('.paypal-buttons')) {
     // document.querySelector('.paypal-buttons').style.height = '40px';
   }
+}
+
+function addPreloader() {
+  document.body.classList.add('lav-preloader');
+  document
+    .querySelector('#__nuxt')
+    .classList.append(
+      '<div class="lds-roller lav-prel"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>'
+    );
 }
