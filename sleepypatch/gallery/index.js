cce03988624a1c8170f53b0eef91ec3310588b77
Alexander Lytovchenko <flopsi69@gmail.com>
2023-05-05 17:18:46 +0300

    fixfix

--- sleepypatch/gallery/index.js
+++ sleepypatch/gallery/index.js
@@ -479,7 +479,7 @@ function addVideo() {
       <div class='container'>
         <div class='lav-video__title'>Kids keeping you awake?</div>
 
-        <video width="100%" controls>
+        <video width="100%" controls muted='muted' playsinline poster='${settings.dir}/img/poster.png'>
           <source src="${settings.dir}/img/wake-ups.mp4" type="video/mp4">
           Your browser doesn't support HTML5 video tag.
         </video>
