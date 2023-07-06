67e4d4e938fcc97b4653b7582017e10554c32d6f
Alexander Lytovchenko <flopsi69@gmail.com>
2023-07-06 19:14:30 +0300

    fix

--- swingu/popup/test.js
+++ swingu/popup/test.js
@@ -1,5 +1,3 @@
-console.log('initTest');
-
 const orig = console.log;
 console.log = function () {
   orig.apply(console, arguments);
@@ -8,7 +6,6 @@ console.log = function () {
 
 // console.log('fire');
 console.log(location.href);
-
 console.log(document.body.innerHTML);
 
 waitFor(
@@ -40,41 +37,33 @@ waitFor(
           background-color: red;
         }
         </style>
+        <div class='lav-point'></div>
       `
     );
 
-    let test = 'test13';
-    console.log('s1: ' + JSON.stringify(sessionStorage.getItem(test)));
-    sessionStorage.setItem(test, 'test');
-    console.log('s2: ' + JSON.stringify(sessionStorage.getItem(test)));
-    console.log('l1: ' + JSON.stringify(localStorage.getItem(test)));
-    localStorage.setItem(test, 'test');
-    console.log('l2: ' + JSON.stringify(localStorage.getItem(test)));
-
-    // <a class='ggg' href='app://upgrade'>u</a>
-    // <a class='ggg' href='/gps/ios/upgrade/player/29378860'>b</a>
-    // <a class='ggg' href='https://webviews.swingu.com/gps/ios/upgrade/player/29378860'>c</a>
-    // <a class='ggg' href='https://webviews.swingu.com/gps/ios/upgrade/player/29378860?deviceType=ios&displayState=default&version=2&accessToken=gAAAAKRGr2H3DEwfUhGwDtNKOKzjtSePSm3fNLIhjk6rZMCA5euhulrhBpyIir9PcXb0hwemGfNuJB8HuNt4XGnacjlrRrpcjlODzIcRyu1zLOqT6KQujdizTnES-07E2TS_7v-rqzmEaSfWx9aQxal7HnlS0wzYivmktVtKavXicW0JFAEAAIAAAABIuCy-dYJv9oLsc0xvOZAP8OjDRCaIYVoy8XcSVNdPi1-lulDw6rttnQkGDYiMmJc48d06jZM1eGj_sc_jyQxSl4-j9Q6qfwaMA7hxg28WeyZsTShp6HpZLEvnlJLzqKPzkMsAIe4cS9Z9vQHC9IDOaCN_H1Qy2QsNoBvz-fWpuATwvOj6lBeOYX9TtYylKdwUyMnwU-cZdGRKZ0o-QVeAX47Zyy07K_0ug-YZs8tB3YNOD832j9VUkF0ns_opvkWruucsLkk_gLodkdoHfTycylZj_wrva5QHqvwLjUAAtYLbQ9BC5w5PmBScKm21UlbRlI73pybuHninRPYCS35J8US-p9nWwWxLLXMHJ5rI9g'>d</a>
-
-    document.body.insertAdjacentHTML(
-      'beforeend',
-      `<div class='lav-point'></div>
-      `
+    console.log(
+      's1: ' + JSON.stringify(sessionStorage.getItem('isRedirectedExp'))
     );
 
-    // setTimeout(() => {
-    // if (!location.href.includes('pgrade/player/29378860')) {
-    //   window.location.href = '/gps/ios/upgrade/player/29378860';
-    // }
-    // }, 1000);
+    if (sessionStorage.getItem('isRedirectedExp') !== 'yes') {
+      console.log('fire');
+      sessionStorage.setItem('isRedirectedExp', 'yes');
+      document
+        .querySelector('[data-crstarget="hypothesis-2-upgrade-target"]')
+        .click();
+    }
+
+    console.log(
+      's2: ' + JSON.stringify(sessionStorage.getItem('isRedirectedExp'))
+    );
   },
   50
 );
 
-// document.body.insertAdjacentHTML('beforeend', "<div class='lav-point'></div>")
 waitFor(
   () => document.querySelector('.upgradePopup .primaryAction'),
   () => {
+    console.log('popup');
     document.body.insertAdjacentHTML(
       'beforeend',
       `
@@ -98,12 +87,8 @@ waitFor(
           background-color: red;
         }
         </style>
+        <div class='lav-point lav-point_2'></div>
       `
-    );
-
-    document.body.insertAdjacentHTML(
-      'beforeend',
-      "<div class='lav-point lav-point_2'></div>"
     );
 
     // document
