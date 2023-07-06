cd76725e09476bd0aa9cda6a6d2c08d602cf89d8
Alexander Lytovchenko <flopsi69@gmail.com>
2023-07-06 18:44:39 +0300

    fix

--- capucinne/pdp_slidein/index.js
+++ capucinne/pdp_slidein/index.js
@@ -2113,12 +2113,18 @@ let klaviyoStep = 1;
 
     if ($el('.lav-paypal')) {
       $el('.lav-paypal').style.display = 'flex';
-      $el('.lav-paypal [data-testid="grid-cell"]')?.style.flexGrow =
-        '1!important';
-      setTimeout(() => {
-        el('.lav-paypal [data-testid="grid-cell"]')?.style.flexGrow =
-        '1!important';
-      }, 1200);
+      if ($el('.lav-paypal [data-testid="grid-cell"]')) {
+        $el('.lav-paypal [data-testid="grid-cell"]').style.flexGrow =
+          '1!important';
+      } else {
+        setTimeout(() => {
+          if ($el('.lav-paypal [data-testid="grid-cell"]')) {
+            $el('.lav-paypal [data-testid="grid-cell"]').style.flexGrow =
+              '1!important';
+          }
+        }, 1500);
+      }
+
       $el('.lav-paypal').addEventListener('click', () => {
         pushDataLayer(
           'new_payments_cart_paypal',
