2e9f2805fea556e3549ae5d12ed4e5674de6dbba
Alexander Lytovchenko <flopsi69@gmail.com>
2023-10-18 22:45:16 +0300

    initExp

---
+++ swingu/30minutes/index.js
@@ -0,0 +1,624 @@
+// const orig = console.log;
+// console.log = function () {
+//   orig.apply(console, arguments);
+//   navigator.sendBeacon(
+//     'https://console.wiredgeese.com/log/',
+//     JSON.stringify(arguments)
+//   );
+// };
+
+console.log('initExp: 30Minutes');
+
+/********* Settings **********/
+const settings = {
+  dir: 'https://flopsi69.github.io/crs/swingu/30minutes',
+  observe: false,
+  clarity: {
+    enable: true,
+    params: ['set', 'exp_stripe_flow', 'variant_1'],
+  },
+  debug: true,
+};
+
+// location.href.includes('hypothesis-3')
+if (location.href.includes('/upgrade/player')) {
+  handleUpgradePage();
+}
+
+function handleUpgradePage() {
+  console.log('initUpgradePage: ' + location.href);
+  //Clarity
+  if (!settings.debug && settings.clarity.enable) {
+    waitFor(
+      () => typeof clarity == 'function',
+      () => {
+        clarity(...settings.clarity.params);
+      }
+    );
+  }
+
+  /*** STYLES / Start ***/
+  const styles = `
+    .antialiased {
+      background: #262626;
+    }
+    .lav-new {
+      padding: 15px;
+    }
+    .lav-offer {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      color: #fff;
+      padding: 6px 12px;
+      border-radius: 10px;
+      background: #313131;
+    }
+    .lav-offer__title {
+      font-size: 17px;
+      font-weight: 700;
+      line-height: 22px;
+      letter-spacing: -0.408px;
+    }
+    .lav-timer {
+      display: flex;
+      font-size: 20px;
+      font-weight: 600;
+      line-height: 24px; 
+    }
+    .lav-timer span + span {
+      margin-left: 2px;
+    }
+    .lav-digit {
+      position: relative;
+      display: flex;
+      justify-content: center;
+      width: 24px;
+      height: 30px;
+      align-items: center;
+      text-align: center;
+      border-radius: 5px;
+      border: 0.5px solid #545454;
+      background: #313131;
+    }
+    .lav-digit:before {
+      content: '';
+      border-radius: 5px 5px 0px 0px;
+      opacity: 0.07;
+      background: var(--White, #FFF);
+      position: absolute;
+      top: 0;
+      left: 0;
+      right: 0;
+      height: 50%;
+    }
+    .lav-timer__caption {
+      display: flex;
+      justify-content: space-between;
+      color: #909094;
+      font-size: 13px;
+      font-weight: 400;
+      line-height: 18px;
+      letter-spacing: -0.078px
+    }
+    .lav-title {
+      color: var(--White, #FFF);
+      font-size: 24px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 32px;
+      margin-top: 15px;
+    }
+    .lav-off {
+      color: var(--Action-Yellow, #FFC803);
+      margin-top: 4px;
+      font-size: 15px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 20px;
+      letter-spacing: -0.5px;
+    }
+    .lav-plate__wrap {
+      margin-top: 12px;
+      border-radius: 10px;
+    }
+    .lav-plate {
+      padding: 15px;
+      border-radius: 10px;
+      background: #313131;
+    }
+    .lav-plate__caption {
+      color: rgba(247, 247, 247, 0.60);
+      font-size: 12px;
+      font-weight: 400;
+      line-height: 16px;
+      margin-top: 15px;
+    }
+
+    .lav-sticky {
+      position: fixed;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      padding: 16px 15px 13px;
+      border-radius: 10px 10px 0px 0px;
+      background: #313131;
+      box-shadow: 0px -2px 12px 0px rgba(1, 1, 1, 0.20);
+      z-index: 99;
+    }
+    .lav-unlock {
+      border-radius: 7px;
+      background: var(--common-medium-primary, #49BB54);
+      padding: 13px 16px;
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 12px 0;
+    }
+    .lav-unlock__mo {
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 21px;
+      letter-spacing: -0.32px;
+      border-radius: 4px;
+      background: #39AC44;
+      padding: 4px 10px;
+      color: var(--common-white, #FFF);
+    }
+    .lav-loader {
+      width: 29px;
+      height: 29px;
+      font-size: 0;
+      line-height: 0;
+      border: 2px solid #FFF;
+      border-bottom-color: transparent;
+      border-radius: 50%;
+      display: inline-block;
+      box-sizing: border-box;
+      animation: rotation 1s linear infinite;;
+    }
+
+    @keyframes rotation {
+      0% {
+          transform: rotate(0deg);
+      }
+      100% {
+          transform: rotate(360deg);
+      }
+    }
+    
+    .lav-sticky__off {
+      color: var(--Font-color, #2B2B2B);
+      font-size: 16px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 21px;
+      letter-spacing: -0.32px;
+      padding: 5px;
+      border-radius: 4px;
+      background: var(--common-medium-secondary, #FFC803);
+      display: flex;
+      align-items: center;
+    }
+
+    .lav-table {
+      margin-top: 16px;
+    }
+    .lav-row {
+      display: flex;
+      color: #fff;
+      border-bottom: 0.5px solid #545454;
+    }
+    .lav-row:not(.lav-row__head) {
+      padding: 7px 10px 7px 0;
+    }
+    .lav-cell:first-child {
+      color: var(--common-white, #FFF);
+      font-size: 15px;
+      font-style: normal;
+      font-weight: 400;
+      line-height: 20px;
+      letter-spacing: -0.5px;
+      flex-grow: 1;
+      text-transform: capitalize;
+    }
+    .lav-cell:nth-child(2), .lav-cell:nth-child(3) {
+      text-align: center;
+      width: 36px;
+      flex-shrink: 0;
+    }
+    .lav-cell img {
+      margin: 2px auto 0;
+    }
+    .lav-row__head {
+      padding: 15px 10px 15px 0;
+      align-items: center;
+    }
+    .lav-row__head .lav-cell:nth-child(1) {
+      color: var(--common-white, #FFF);
+      font-size: 17px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 22px;
+      letter-spacing: -0.408px;
+    }
+    .lav-row__head .lav-cell:nth-child(2) {
+      color: var(--neutral-grey-5009-cabb-5, #9CABB5);
+      text-align: center;
+      font-size: 15px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 20px;
+      letter-spacing: -0.5px;
+    }
+    .lav-row__head .lav-cell:nth-child(3) {
+      color: var(--common-medium-primary, #49BB54);
+      text-align: center;
+      font-size: 15px;
+      font-style: normal;
+      font-weight: 600;
+      line-height: 20px;
+      letter-spacing: -0.5px;
+    }
+    .lav-toggler {
+      display: flex;
+      align-items: center;
+      color: var(--Green-Main, #49BB54);
+      font-size: 15px;
+      font-style: normal;
+      font-weight: 700;
+      line-height: 20px;
+      letter-spacing: -0.24px;
+      margin-top: 15px;
+    }
+    .lav-toggler svg {
+      margin-left: 8px;
+      transition: 0.35s;
+      position: relative;
+      top: 1px;
+    }
+    .lav-toggler.active svg {
+      transform: rotate(180deg);
+    }
+    .lav-table:not(.active) .lav-row:nth-child(1n+6) {
+      display: none;
+    }
+    .lav-close {
+      margin-bottom: 16px;
+    }
+    [dusk="gps-upgrade-container"] {
+      display: none;
+    }
+  `;
+
+  const stylesEl = document.createElement('style');
+  stylesEl.innerHTML = styles;
+
+  waitFor(
+    () => document.head,
+    () => {
+      document.head.appendChild(stylesEl);
+    },
+    100
+  );
+  /*** STYLES / End ***/
+
+  const html = `
+    <div class="lav-new">
+      <div class="lav-close">
+        <svg class="inline-block" width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M11.8799 22.2012C17.874 22.2012 22.8369 17.2275 22.8369 11.2441C22.8369 5.25 17.8633 0.287109 11.8691 0.287109C5.88574 0.287109 0.922852 5.25 0.922852 11.2441C0.922852 17.2275 5.89648 22.2012 11.8799 22.2012ZM11.8799 20.375C6.80957 20.375 2.75977 16.3145 2.75977 11.2441C2.75977 6.17383 6.79883 2.11328 11.8691 2.11328C16.9395 2.11328 21.0107 6.17383 21.0107 11.2441C21.0107 16.3145 16.9502 20.375 11.8799 20.375ZM8.20605 15.7881C8.45312 15.7881 8.66797 15.6914 8.8291 15.5195L11.8691 12.4688L14.9307 15.5195C15.0918 15.6807 15.2959 15.7881 15.543 15.7881C16.0156 15.7881 16.4023 15.4014 16.4023 14.918C16.4023 14.6709 16.3164 14.4775 16.1445 14.3057L13.0938 11.2549L16.1553 8.18262C16.3379 8 16.4131 7.81738 16.4131 7.58105C16.4131 7.1084 16.0264 6.72168 15.5537 6.72168C15.3281 6.72168 15.1455 6.80762 14.9629 6.99023L11.8691 10.0518L8.80762 7.00098C8.64648 6.8291 8.45312 6.74316 8.20605 6.74316C7.7334 6.74316 7.34668 7.11914 7.34668 7.5918C7.34668 7.82812 7.44336 8.03223 7.60449 8.19336L10.6553 11.2549L7.60449 14.3164C7.44336 14.4775 7.34668 14.6816 7.34668 14.918C7.34668 15.4014 7.7334 15.7881 8.20605 15.7881Z" fill="#ffffff"></path></svg>
+      </div>
+
+      <div class="lav-offer">
+        <div class="lav-offer__title">Offer available for:</div>
+
+        <div class="lav-offer__info">
+          <div class="lav-timer">
+            <span class='lav-digit'>-</span>
+            <span class='lav-digit'>-</span>
+            <span class='lav-divider'>:</span>
+            <span class='lav-digit'>-</span>
+            <span class='lav-digit'>-</span>
+          </div>
+          <div class="lav-timer__caption">
+            <span>Minutes</span>
+            <span>Seconds</span>
+          </div>
+        </div>
+      </div>
+
+      <div class="lav-title">An Exclusive Offer for You!</div>
+      <div class="lav-off">40% Off SwingU Pro Yearly</div>
+
+      <div class="lav-plate__wrap">
+        <img class="lav-plate__img" src="${settings.dir}/img/plate-bg.png" alt="" />
+        
+        <div class="lav-plate">
+          <div class="lav-plate__caption">July 16, 2023 - July 16, 2024: $99.00 auto-renewal. No commitment. Cancel anytime.</div>
+        </div>
+      </div>
+    </div>
+  `;
+
+  /********* Custom Code **********/
+  waitFor(
+    () => document.querySelector('.antialiased'),
+    () => {
+      init();
+    },
+    100
+  );
+
+  function init() {
+    console.log('init');
+
+    pushDataLayer(
+      'exp_stripe_v_utsp_ps',
+      `Popup - ${
+        document.querySelector('.lav-table.active') ? 'extended' : 'short'
+      }`,
+      'Visibility',
+      'Upgrade to SwingU Pro'
+    );
+
+    document
+      .querySelector('.antialiased')
+      .insertAdjacentHTML('afterbegin', html);
+
+    document.querySelector('.lav-close').addEventListener('click', function () {
+      pushDataLayer(
+        'exp_stripe_c_utsp_ps',
+        `Popup - ${
+          document.querySelector('.lav-table.active') ? 'extended' : 'short'
+        }`,
+        'Close',
+        'Upgrade to SwingU Pro'
+      );
+      document
+        .querySelector('[dusk="gps-upgrade-container"] .absolute')
+        .click();
+    });
+
+    drawTable();
+    addSticky();
+    startCountdown();
+  }
+
+  function startCountdown(duration = 30) {
+    const elements = document.querySelectorAll('.lav-digit');
+    let timer = duration * 60; //sceconds
+    const countdownInterval = setInterval(function () {
+      const minutesTens = Math.floor(timer / 60 / 10);
+      const minutesOnes = Math.floor(timer / 60) % 10;
+      const secondsTens = Math.floor((timer % 60) / 10);
+      const secondsOnes = timer % 10;
+
+      elements[0].textContent = minutesTens;
+      elements[1].textContent = minutesOnes;
+      elements[2].textContent = secondsTens;
+      elements[3].textContent = secondsOnes;
+
+      if (--timer < 0) {
+        clearInterval(countdownInterval);
+        alert('Countdown timer has expired!');
+      }
+    }, 1000);
+  }
+
+  function addSticky() {
+    const el = document.createElement('div');
+    el.classList.add('lav-sticky');
+
+    el.innerHTML = `
+      <div class='lav-sticky__head'>
+        <div class='lav-sticky__off'>40% off</div>
+      </div>
+
+      <div class='lav-sticky__body'>
+        <div class='lav-unlock'>
+          <img class="lav-unlock__img" src="${settings.dir}/img/logo-white.svg" alt="" />
+          <div class="lav-unlock__mo">$4.99/mo</div>
+        </div>
+      </div>
+    `;
+
+    document
+      .querySelector('.antialiased')
+      .insertAdjacentElement('beforeend', el);
+
+    handleStickyOffset();
+    window.addEventListener('resize', handleStickyOffset);
+
+    document
+      .querySelector('.lav-unlock:not(.lav-clicked)')
+      .addEventListener('click', function () {
+        this.classList.add('lav-clicked');
+        document.querySelector('.lav-unlock__mo').classList.add('lav-loader');
+        if (document.querySelector('[data-stripe-checkout-url]')) {
+          const url = document.querySelector('[data-stripe-checkout-url]')
+            .dataset.stripeCheckoutUrl;
+
+          pushDataLayer(
+            'exp_stripe_v_utsp_usp',
+            'Unlock SwingPro',
+            'Button',
+            'Upgrade to SwingU Pro'
+          );
+
+          const link = document.createElement('a');
+          link.href = url;
+          link.click();
+        }
+      });
+
+    setTimeout(() => {
+      pushDataLayer(
+        'exp_stripe_v_utsp_ba',
+        'Billed annually',
+        'Visibility',
+        'Upgrade to SwingU Pro'
+      );
+    }, 2000);
+  }
+
+  function handleStickyOffset() {
+    document.querySelector('.lav-new').style.paddingBottom = `${
+      document.querySelector('.lav-sticky').offsetHeight + 30
+    }px`;
+  }
+
+  function drawTable() {
+    const table = document.createElement('div');
+    table.classList.add('lav-table');
+
+    table.innerHTML = `
+      <div class="lav-row lav-row__head">
+        <div class="lav-cell">What you get with Pro</div>
+        <div class="lav-cell">Free</div>
+        <div class="lav-cell">Pro</div>
+      </div>
+    `;
+
+    const items = [
+      { benefit: '"Plays Like" Distances' },
+      { benefit: 'Club Recommendations' },
+      { benefit: 'Green reading maps' },
+      { benefit: 'Advanced stats & strokes gained' },
+      { benefit: 'Distances to greens and hazards', free: true },
+      {
+        benefit: 'GPS Rangefinder and scorecard for every course in the world',
+        free: true,
+      },
+      { benefit: 'Ad free experience' },
+      { benefit: 'Wind Speed & Elevation' },
+      { benefit: 'Enhanced Scorecard & Stats' },
+      { benefit: 'Green-Reading Maps For 13,000+ Courses' },
+      { benefit: 'SwingU Handicap', free: true },
+      { benefit: 'Shot Tracking', free: true },
+      { benefit: 'Clubhouse Content Feed', free: true },
+      { benefit: 'Giveaway Eligibility' },
+      { benefit: 'Strokes Gained Stats vs.<br/> Your Target Handicap' },
+      {
+        benefit:
+          'Handicap Index for Each Facet of Your Game (Driving, Approach, Chip/Pitch, Bunker & Putting)',
+      },
+      { benefit: 'Post-Round #1 Game<br/> Improvement Priority' },
+      { benefit: 'Post-Round Personalized,<br/> Prescriptive Drill' },
+      {
+        benefit:
+          'Extensive Library of Performance Practice Drills and Informative Videos',
+      },
+    ];
+
+    items.forEach((item) => {
+      const row = document.createElement('div');
+      row.classList.add('lav-row');
+
+      row.innerHTML = `
+        <div class="lav-cell">${item.benefit}</div>
+        <div class="lav-cell"><img src='${settings.dir}/img/${
+        item.free ? 'check-gray.svg' : 'uncheck.svg'
+      }' /></div>
+        <div class="lav-cell"><img src='${
+          settings.dir
+        }/img/check-green.svg' /></div>
+      `;
+
+      table.appendChild(row);
+    });
+
+    document
+      .querySelector('.lav-plate')
+      .insertAdjacentElement('afterbegin', table);
+
+    const toggler = document.createElement('div');
+    toggler.classList.add('lav-toggler');
+    toggler.innerHTML = `<span>View all features</span> <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 14 14" fill="none">
+    <path d="M7 10C7.17281 10 7.3318 9.93865 7.45622 9.82209L12.8203 4.96319C12.9378 4.85276 13 4.71779 13 4.55828C13 4.2454 12.7304 4 12.371 4C12.1982 4 12.0392 4.06135 11.9217 4.15951L7 8.62577L2.07834 4.15951C1.96083 4.06135 1.80875 4 1.62903 4C1.26958 4 1 4.2454 1 4.55828C1 4.71779 1.06221 4.85276 1.18664 4.96319L6.54378 9.82209C6.67511 9.93865 6.82719 10 7 10Z" fill="#49BB54"/>
+    </svg>`;
+
+    toggler.addEventListener('click', function () {
+      if (document.querySelector('.lav-table.active')) {
+        pushDataLayer(
+          'exp_stripe_b_utsp_vtf',
+          'View top features',
+          'Button',
+          'Upgrade to SwingU Pro'
+        );
+      } else {
+        pushDataLayer(
+          'exp_stripe_b_utsp_vaf',
+          'View all features',
+          'Button',
+          'Upgrade to SwingU Pro'
+        );
+      }
+
+      table.classList.toggle('active');
+      toggler.classList.toggle('active');
+      toggler.querySelector('span').innerText = table.classList.contains(
+        'active'
+      )
+        ? 'View top features'
+        : 'View all features';
+    });
+
+    table.insertAdjacentElement('afterend', toggler);
+  }
+}
+
+// *** Utils *** //
+function waitFor(condition, cb, ms = 1000) {
+  if (condition()) {
+    if (typeof cb == 'function') cb();
+    return;
+  }
+
+  let interval = setInterval(function () {
+    if (condition()) {
+      clearInterval(interval);
+      if (typeof cb == 'function') cb();
+    }
+  }, ms);
+}
+
+// Alalytic 4
+function pushDataLayer(name = '', desc = '', type = '', loc = '') {
+  try {
+    var objData = {
+      event: 'event-to-ga4',
+      event_name: name,
+      event_desc: desc,
+      event_type: type,
+      event_loc: loc,
+    };
+    console.log('eventFire', objData);
+    if (!settings.debug) {
+      dataLayer.push(objData);
+    }
+  } catch (e) {
+    console.log('Event Error:', e);
+  }
+}
+
+// Mutation Observer
+function initObserver(cb) {
+  let observer = new MutationObserver((mutations) => {
+    for (let mutation of mutations) {
+      for (let node of mutation.addedNodes) {
+        if (!(node instanceof HTMLElement)) continue;
+
+        cb(node);
+      }
+    }
+  });
+
+  waitFor(
+    () => document.body,
+    () => {
+      observer.observe(document.body, { childList: true, subtree: true });
+    },
+    100
+  );
+}
