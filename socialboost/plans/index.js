de11aeaedc8cead72ebe3e6b3d65ecea3a7923eb
Alexander Lytovchenko <flopsi69@gmail.com>
2023-11-16 17:09:17 +0200

    fix

--- socialboost/plans/index.js
+++ socialboost/plans/index.js
@@ -1168,12 +1168,12 @@
   }
 
   if (navigation) {
-    navigation.addEventListener('navigate', (e) => {
-      if (e.navigationType === 'push') {
-        console.log('fireNavigate', e);
-        setTimeout(init, 300);
-      }
-    });
+    // navigation.addEventListener('navigate', (e) => {
+    //   if (e.navigationType === 'push') {
+    //     console.log('fireNavigate', e);
+    //     setTimeout(init, 300);
+    //   }
+    // });
   }
   window.addEventListener('popstate', (e) => {
     console.log('firePopState', e);
