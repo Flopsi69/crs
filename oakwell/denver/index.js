29fa051ea668ec55185a14fd9a4ebde99a8ce328
Alexander Lytovchenko <flopsi69@gmail.com>
2025-04-11 21:55:57 +0700

    update

--- oakwell/denver/index.js
+++ oakwell/denver/index.js
@@ -585,7 +585,8 @@
             _$('.tabs-section-wrap.section-prices') &&
             _$('#private_party__form') &&
             (!document.body.style.opacity ||
-              document.body.style.opacity === '1')
+              document.body.style.opacity === '1' ||
+              parseFloat(document.body.style?.opacity) === '1') > 0.7
           )
         },
         () => {
