952c2269923b248840ea71a1589e0314ab316778
Alexander Lytovchenko <flopsi69@gmail.com>
2025-06-12 20:09:03 +0700

    update

--- buzzpatch/improveSelectPackages/index.js
+++ buzzpatch/improveSelectPackages/index.js
@@ -6,7 +6,7 @@
     // dir: 'http://127.0.0.1:5500/buzzpatch/improveSelectPackages',
     dir: 'https://flopsi69.github.io/crs/buzzpatch/improveSelectPackages',
     clarity: ['set', 'exp_buzzpatch_7', 'variant_1'],
-    debug: true
+    debug: false
   }
 
   // const orig = console.log
