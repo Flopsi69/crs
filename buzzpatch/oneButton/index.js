b4c82663bb04a4662cd1172601a6251661421b0a
Alexander Lytovchenko <flopsi69@gmail.com>
2025-07-22 22:36:33 +0700

    init new exp

---
+++ buzzpatch/oneButton/index.js
@@ -0,0 +1,294 @@
+;(function () {
+  console.debug('*** Experiment started ***')
+
+  // Config for Experiment
+  const config = {
+    // dir: 'http://127.0.0.1:5500/<dir>/<project>',
+    dir: 'https://flopsi69.github.io/crs/<dir>/<project>',
+    clarity: ['set', 'exp_buzzpatch_8', 'variant_1'],
+    debug: false
+  }
+
+  // const orig = console.log
+  // console.log = function (...args) {
+  //   orig.apply(console, ['Debug:', ...args])
+  // }
+
+  // Styles for Experiment
+  const styles = /* css */ `
+    #getNow .form-group {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      gap: 10px 15px;
+      max-width: 316px;
+      margin: 10px auto!important;;
+    }
+    #getNow .form-group .js-packs {
+      max-width: initial;
+      margin: 0;
+    }
+    #getNow .form-group .js-packs:first-child {
+      grid-column: span 2;
+    }
+    #getNow .form-group .js-packs:last-child {
+      display: none;
+    }
+  `
+
+  const stylesEl = document.createElement('style')
+  stylesEl.classList.add('exp-styles')
+  stylesEl.innerHTML = styles
+
+  // *** Logic *** //
+  initExp()
+
+  async function initExp() {
+    await waitFor(() => document.head && document.body, false, { ms: 20 })
+
+    document.head.appendChild(stylesEl)
+
+    console.debug('** InitExp **')
+  }
+
+  // *** HELPERS *** //
+
+  // Waiting for loading by condition
+  async function waitFor(condition, cb = false, customConfig = {}) {
+    const config = {
+      ms: 500, // repeat each 0.5 second if condition is false
+      limit: 10, // limit in second seconds
+
+      ...customConfig
+    }
+
+    if (typeof condition === 'function') {
+      if (condition()) {
+        if (typeof cb === 'function') cb()
+        return
+      }
+
+      return new Promise((resolve) => {
+        let limit = config.limit * 1000
+        const interval = setInterval(function () {
+          if (condition() || limit <= 0) {
+            clearInterval(interval)
+            if (limit > 0 && typeof cb === 'function') cb()
+            resolve()
+          }
+          limit -= config.ms
+        }, config.ms)
+      })
+    }
+
+    if (condition.startsWith('.') || condition.startsWith('#')) {
+      if (_$(condition)) {
+        if (typeof cb === 'function') cb(_$(condition))
+        return
+      }
+
+      return new Promise((resolve) => {
+        const observer = new MutationObserver((mutations, observer) => {
+          if (_$(condition)) {
+            if (typeof cb === 'function') cb(_$(condition))
+            observer.disconnect()
+            resolve()
+          }
+        })
+
+        observer.observe(document, { childList: true, subtree: true })
+      })
+    }
+  }
+
+  // Mutation Observer
+  function initMutation(observeEl = document.body, cbAdded, cbRemoved) {
+    const el = typeof observeEl === 'string' ? _$(observeEl) : observeEl
+
+    if (!el) return
+
+    let observer = new MutationObserver((mutations, observer) => {
+      for (let mutation of mutations) {
+        if (typeof cbAdded === 'function') {
+          for (let node of mutation.addedNodes) {
+            if (!(node instanceof HTMLElement)) continue
+            cbAdded(node, observer)
+          }
+        }
+
+        if (typeof cbRemoved === 'function') {
+          for (let node of mutation.removedNodes) {
+            if (!(node instanceof HTMLElement)) continue
+            cbRemoved(node, observer)
+          }
+        }
+      }
+    })
+
+    observer.observe(el, { childList: true, subtree: true })
+
+    return observer
+  }
+
+  // Intersection Observer
+  function initIntersection(observeEl, cb, customConfig) {
+    const el = typeof observeEl === 'string' ? _$(observeEl) : observeEl
+
+    if (!el || typeof cb !== 'function') return
+
+    const config = {
+      root: null,
+      threshold: 0.3, // 0 - 1 | A threshold of 1.0 means that when 100% of the target is visible within the element specified by the root option, the callback is invoked.
+      ...customConfig
+    }
+
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach((entry) => {
+        cb(entry, observer)
+      })
+    }, config)
+
+    observer.observe(el)
+
+    return observer
+  }
+
+  function focusTimeEvent(el, cb, viewElementProcent = 0.1) {
+    let entryTime = 0
+    initIntersection(
+      el,
+      ({ isIntersecting, time }) => {
+        if (isIntersecting) {
+          entryTime = time
+        } else if (entryTime) {
+          const diffTime = +((time - entryTime) / 1000).toFixed(1)
+          cb(diffTime + 's')
+          entryTime = 0
+        }
+      },
+      { threshold: viewElementProcent }
+    )
+  }
+
+  function visibilityEvent(el, cb, customConfig = {}) {
+    const config = {
+      threshold: 0.3,
+      ...customConfig,
+      timer: null
+    }
+    initIntersection(
+      el,
+      ({ isIntersecting, target }, observer) => {
+        // console.log(target, isIntersecting);
+        if (isIntersecting) {
+          config.timer = setTimeout(() => {
+            if (isElementInViewport(target)) {
+              cb()
+              observer.disconnect()
+            }
+          }, 3000)
+        } else {
+          clearTimeout(config.timer)
+        }
+      },
+      config
+    )
+  }
+
+  // Artificial delay
+  function delay(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+  }
+
+  // Check if element in viewport
+  function isElementInViewport(selector) {
+    const el = typeof selector === 'string' ? _$(selector) : selector
+
+    if (!el) return false
+
+    const rect = el.getBoundingClientRect()
+    const windowHeight =
+      window.innerHeight || document.documentElement.clientHeight
+
+    return (
+      rect.top + rect.height * 0.3 < windowHeight &&
+      rect.bottom > rect.height * 0.3
+    )
+    // return (
+    //   rect.top >= 0 &&
+    //   rect.left >= 0 &&
+    //   rect.bottom <=
+    //     (window.innerHeight || document.documentElement.clientHeight) &&
+    //   rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    // );
+  }
+
+  // Shordcode for selectors
+  function _$(selector, context = document) {
+    return context.querySelector(selector)
+  }
+  function _$$(selector, context = document, toSimpleArray = false) {
+    const arr = context.querySelectorAll(selector)
+
+    return toSimpleArray ? Array.from(arr) : arr
+  }
+
+  // GA 4 events
+  function pushDataLayer(name = '', desc = '', type = '', loc = '') {
+    window.dataLayer = window.dataLayer || []
+
+    try {
+      const event = {
+        event: 'event-to-ga4',
+        event_name: name,
+        event_desc: desc,
+        event_type: type,
+        event_loc: loc
+      }
+
+      console.debug('** GA4 Event **', event)
+
+      if (!config.debug) {
+        dataLayer.push(event)
+      }
+    } catch (e) {
+      console.log('** GA4 Error **', e)
+    }
+  }
+
+  // Slider
+  function connectSplide() {
+    const sliderStyles = document.createElement('link')
+    sliderStyles.rel = 'stylesheet'
+    sliderStyles.href =
+      'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/css/splide-core.min.css'
+    document.head.appendChild(sliderStyles)
+
+    let sliderScript = document.createElement('script')
+    sliderScript.src =
+      'https://cdn.jsdelivr.net/npm/@splidejs/splide@4.1.4/dist/js/splide.min.js'
+    document.head.appendChild(sliderScript)
+  }
+
+  // *** Exp BG process *** //
+
+  //Clarity
+  if (
+    !config.debug &&
+    Array.isArray(config.clarity) &&
+    config.clarity.length === 3
+  ) {
+    waitFor(
+      () => typeof clarity == 'function',
+      () => {
+        clarity(...config.clarity)
+      }
+    )
+  }
+
+  // Svg objects
+  function getSvg(name) {
+    const svgObj = {}
+
+    return svgObj[name]
+  }
+})()
