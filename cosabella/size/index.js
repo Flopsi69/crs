f2042be4f6760a77dfc7d7bb32e58b0d89df38d4
Alexander Lytovchenko <flopsi69@gmail.com>
2025-08-21 18:13:18 +0700

    add sticky desktop

--- cosabella/size/index.js
+++ cosabella/size/index.js
@@ -63,7 +63,7 @@
   .template-product .js-product-info .form--product__field--Size + .form--product__add-to-cart {
     margin-top: 6px;
   }
-  .template-product .form--product__field--Size span.form--product__dropdown-label.js-dropdown-label {
+  #mobileSizeSelector span.form--product__dropdown-label.js-dropdown-label {
     color: #0A0A12;
     font-size: 14.125px;
     font-style: normal;
@@ -93,9 +93,11 @@
     line-height: 20px;
     margin-top: 6px;
   }
-  #mobileSizeSelector {
-    padding-right: 0;
-    padding-left: 160px;
+  @media(max-width: 767px) {
+    #mobileSizeSelector {
+      padding-right: 0;
+      padding-left: 160px;
+    }
   }
   #mobileAddToCartBtn {
     padding-left: 0;
@@ -131,6 +133,93 @@
   #mobileAddToCartBtn .atc-label {
     display: none;
   }
+
+  .lav-sticky {
+    position: fixed;
+    transform: translateY(100%);
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 6;
+    display: none;
+    justify-content: space-between;
+    align-items: center;
+    border-top: 1px solid rgba(215, 133, 122, 0.20);
+    background: #FFF;
+    padding: 12px 80px;
+    transition: .2s;
+  }
+  .lav-sticky--show {
+    transform: translateY(0%);
+  }
+  @media(min-width: 768px) {
+    .lav-sticky {
+      display: flex;
+    }
+  }
+  .lav-sticky__info {}
+  .lav-sticky__title {
+    color: #000;
+    font-family: Cambon;
+    font-size: 30px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 35.98px;
+    letter-spacing: -0.4px;
+  }
+  .lav-sticky__rate {
+    margin-top: 4px;
+  }
+  @media(min-width: 768px) {
+    .template-product .lav-sticky__size .form--product__dropdown-toggle-wrapper2 {
+      display: flex!important;
+    }
+  }
+  .lav-sticky__btn  {
+    position: relative;
+  }
+  .lav-sticky__btn .btn {
+    flex-wrap: nowrap;
+    min-width: 320px;
+    padding-left: 0;
+    padding-right: 0;
+  }
+  .lav-sticky__btn-overlay {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 1;
+    background-color: transparent;
+    cursor: pointer;
+  }
+  .lav-sticky__actions {
+    display: flex;
+    align-items: center;
+    gap: 18px;
+  }
+  .lav-sticky__size {
+    width: 280px;
+  }
+  .lav-sticky__size #mobileSizeSelector {
+    max-width: 280px!important;
+  }
+  .lav-sticky__size .form--product__dropdown-wrapper {
+    bottom: calc(100% - 1px);
+    top: initial;
+    width: 100%;
+  }
+  .lav-sticky__size .form--product__dropdown-label-wrapper.select-error {
+    -webkit-animation: shake .2s ease-in-out 0s 2;
+    animation: shake .2s ease-in-out 0s 2;
+    -webkit-box-shadow: 0 0 .5em red;
+    box-shadow: 0 0 .5em red;
+  }
+  .lav-sticky--show-wrap #attentive_creative {
+    left: 100px!important;
+    bottom: 100px!important;
+  }
 `
 
   const stylesEl = document.createElement('style')
@@ -150,6 +239,115 @@
     waitFor('.form--product__field--Size', handleSize)
     waitFor('.product__wishlist-button', addFreeReturn)
     waitFor('#mobileSizeSelector', handleSticky)
+    waitFor(
+      () => _$('.form--product__title') && _$('.stamped-product-reviews-badge'),
+      () => {
+        setTimeout(() => {
+          addDesktopSticky()
+        }, 1000)
+      }
+    )
+  }
+
+  function addDesktopSticky() {
+    if (window.innerWidth < 768) return
+
+    const title = _$('.form--product__title').innerText
+    const el = document.createElement('div')
+    el.classList.add('lav-sticky')
+
+    el.innerHTML = /*html*/ `
+      <div class='lav-sticky__info'>
+        <div class='lav-sticky__title'>${title}</div>
+        <div class='lav-sticky__rate'></div>
+      </div>
+      <div class="lav-sticky__actions">
+        <div class="lav-sticky__size"></div>
+        <div class="lav-sticky__btn"></div>
+      </div>
+    `
+
+    const reviewsEl = _$(
+      '.form--product .stamped-product-reviews-badge'
+    ).cloneNode(true)
+
+    el.querySelector('.lav-sticky__rate').appendChild(reviewsEl)
+
+    _$('#mobileAddToCartBtn').insertAdjacentElement('afterend', el)
+
+    el.querySelector('.lav-sticky__size').appendChild(_$('#mobileSizeSelector'))
+
+    updateDesktopStickyButton()
+
+    initMutation(
+      '.form--product__actions.form--product__add-to-cart:not(#mobileAddToCartBtn)',
+      (node) => {
+        updateDesktopStickyButton()
+      }
+    )
+
+    const handleStickyVisibility = () => {
+      const productForm = _$(
+        '.template-product .js-product-info .form--product'
+      )
+      if (!productForm) return
+
+      const boundary = productForm.getBoundingClientRect()
+      const isFormVisible = boundary.top + boundary.height < 0
+
+      el.classList.toggle('lav-sticky--show', isFormVisible)
+      document.body.classList.toggle('lav-sticky--show-wrap', isFormVisible)
+    }
+
+    let scrollTimeout = null
+    window.addEventListener(
+      'scroll',
+      () => {
+        if (scrollTimeout) {
+          clearTimeout(scrollTimeout)
+        }
+
+        scrollTimeout = setTimeout(handleStickyVisibility, 10)
+      },
+      { passive: true }
+    )
+  }
+
+  function updateDesktopStickyButton() {
+    const addToCartEl = _$('#mobileAddToCartBtn [type="button"]').outerHTML
+
+    _$(
+      '.lav-sticky__size .form--product__dropdown-label-wrapper.select-error'
+    )?.classList.remove('select-error')
+
+    _$('.lav-sticky__btn').innerHTML = addToCartEl
+    if (_$('#pre-select-atc:not(.hide)')) {
+      _$('.lav-sticky__btn').insertAdjacentHTML(
+        'beforeend',
+        '<div class="lav-sticky__btn-overlay"></div>'
+      )
+    }
+
+    if (_$('.lav-sticky__size .js-dropdown-label')) {
+      _$('.lav-sticky__size .js-dropdown-label').innerText = _$(
+        '.template-product .form--product__field--Size span.form--product__dropdown-label.js-dropdown-label'
+      )?.innerText
+    }
+
+    _$('.lav-sticky__btn-overlay')?.addEventListener('click', () => {
+      if (_$('#pre-select-atc:not(.hide)')) {
+        _$(
+          '.lav-sticky__size .form--product__dropdown-label-wrapper'
+        )?.classList.add('select-error')
+
+        return
+      }
+    })
+
+    _$('.lav-sticky__btn button').addEventListener('click', () => {
+      console.log('clicked')
+      // _$('.form--product__actions .js_product_btn').click()
+    })
   }
 
   function handleSticky() {
@@ -319,14 +517,16 @@
       for (let mutation of mutations) {
         if (typeof cbAdded === 'function') {
           for (let node of mutation.addedNodes) {
-            if (!(node instanceof HTMLElement)) continue
+            // console.log('muta added:', node)
+            // if (!(node instanceof HTMLElement)) continue
             cbAdded(node, observer)
           }
         }
 
         if (typeof cbRemoved === 'function') {
           for (let node of mutation.removedNodes) {
-            if (!(node instanceof HTMLElement)) continue
+            // console.log('muta removed:', node)
+            // if (!(node instanceof HTMLElement)) continue
             cbRemoved(node, observer)
           }
         }
