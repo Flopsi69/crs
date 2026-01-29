5f01d37f1f0eaaf34b1f33e283782efae296fc86
Alexander Lytovchenko <flopsi69@gmail.com>
2026-01-29 22:05:40 +0700

    update

--- petals/pdp_improvment/index-color-ver2.js
+++ petals/pdp_improvment/index-color-ver2.js
@@ -1553,7 +1553,7 @@ ul.owlCustomCarousel {
 
           setTimeout(() => {
             highlightEl.classList.remove('lav-colors__note--highlight_active')
-          }, 1700);
+          }, 3400);
 
           return
         }
