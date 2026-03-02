29b822e8c7302dac188e61efa32fbd642fdd6968
Alexander Lytovchenko <flopsi69@gmail.com>
2026-03-02 21:33:45 +0700

    update

--- houseofworktops/reels/index.js
+++ houseofworktops/reels/index.js
@@ -1259,7 +1259,7 @@ function getSvg(name) {
       </svg>
     `,
     'fullscreen': /* html */ `
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none"><script xmlns=""/>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none">
       <path d="M9.00002 3.99997H4.00004L4 8.99999M20 8.99999V4L15 3.99997M15 20H20L20 15M4 15L4 20L9.00002 20" stroke="#000000" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
     </svg>
     `
