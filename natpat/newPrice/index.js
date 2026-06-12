2af9487dd3462537033a7667e324e4d350ed6fdc
Alexander Lytovchenko <flopsi69@gmail.com>
2026-06-12 19:02:53 +0700

    initExp

---
+++ natpat/newPrice/index.js
@@ -0,0 +1,281 @@
+console.debug('*** Experiment started ***')
+
+// Config for Experiment
+const config = {
+  // dir: 'http://127.0.0.1:5500/<dir>/<project>',
+  dir: 'https://flopsi69.github.io/crs/<dir>/<project>',
+  clarity: ['set', '', 'variant_1'],
+  debug: true,
+  altId: '45637565612076'
+}
+
+// const orig = console.log
+// console.log = function (...args) {
+//   orig.apply(console, ['Debug:', ...args])
+// }
+
+// Styles for Experiment
+const styles = /* css */ ``
+
+const stylesEl = document.createElement('style')
+stylesEl.classList.add('exp-styles')
+stylesEl.innerHTML = styles
+
+// *** Logic *** //
+initExp()
+
+async function initExp() {
+  await waitFor(() => document.head && document.body, false, { ms: 20 })
+  waitFor(() => _$('[data-crs-alternative]'), handleNewPrice)
+
+  document.head.appendChild(stylesEl)
+
+  console.debug('** InitExp **')
+}
+
+function handleNewPrice() {
+  _$$('[data-crs-alternative]').forEach((el) => {
+    const price = el.dataset.crsAlternative
+
+    if (price) {
+      el.textContent = price
+    }
+  });
+
+  _$$('[data-crs-src-checkout]').forEach((el) => {
+    const href = el.dataset.crsSrcCheckout;
+    el.dataset.srcCheckout = href;
+
+    if (el.classList.contains('active') && el.closest('.lp-mkv2-chk-col-right')) {
+      el.closest('.lp-mkv2-chk-col-right').querySelector('.lp-mkv2-btn-primary.lp-mkv2-btn-full.lp-mkv2-chk-btn').href = href;
+    }
+  });
+}
+
+// *** HELPERS *** //
+
+// Waiting for loading by condition
+async function waitFor(condition, cb = false, customConfig = {}) {
+  const config = {
+    ms: 500, // repeat each 0.5 second if condition is false
+    limit: 10, // limit in second seconds
+
+    ...customConfig
+  }
+
+  if (typeof condition === 'function') {
+    if (condition()) {
+      if (typeof cb === 'function') cb()
+      return
+    }
+
+    return new Promise((resolve) => {
+      let limit = config.limit * 1000
+      const interval = setInterval(function () {
+        if (condition() || limit <= 0) {
+          clearInterval(interval)
+          if (limit > 0 && typeof cb === 'function') cb()
+          resolve()
+        }
+        limit -= config.ms
+      }, config.ms)
+    })
+  }
+
+  if (condition.startsWith('.') || condition.startsWith('#')) {
+    if (_$(condition)) {
+      if (typeof cb === 'function') cb(_$(condition))
+      return
+    }
+
+    return new Promise((resolve) => {
+      const observer = new MutationObserver((mutations, observer) => {
+        if (_$(condition)) {
+          if (typeof cb === 'function') cb(_$(condition))
+          observer.disconnect()
+          resolve()
+        }
+      })
+
+      observer.observe(document, { childList: true, subtree: true })
+    })
+  }
+}
+
+// Mutation Observer
+function initMutation(observeEl = document.body, cbAdded, cbRemoved) {
+  const el = typeof observeEl === 'string' ? _$(observeEl) : observeEl
+
+  if (!el) return
+
+  let observer = new MutationObserver((mutations, observer) => {
+    for (let mutation of mutations) {
+      if (typeof cbAdded === 'function') {
+        for (let node of mutation.addedNodes) {
+          if (!(node instanceof HTMLElement)) continue
+          cbAdded(node, observer)
+        }
+      }
+
+      if (typeof cbRemoved === 'function') {
+        for (let node of mutation.removedNodes) {
+          if (!(node instanceof HTMLElement)) continue
+          cbRemoved(node, observer)
+        }
+      }
+    }
+  })
+
+  observer.observe(el, { childList: true, subtree: true })
+
+  return observer
+}
+
+// Intersection Observer
+function initIntersection(observeEl, cb, customConfig) {
+  const el = typeof observeEl === 'string' ? _$(observeEl) : observeEl
+
+  if (!el || typeof cb !== 'function') return
+
+  const config = {
+    root: null,
+    threshold: 0.3, // 0 - 1 | A threshold of 1.0 means that when 100% of the target is visible within the element specified by the root option, the callback is invoked.
+    ...customConfig
+  }
+
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach((entry) => {
+      cb(entry, observer)
+    })
+  }, config)
+
+  observer.observe(el)
+
+  return observer
+}
+
+function focusTimeEvent(el, cb, viewElementProcent = 0.1) {
+  let entryTime = 0
+  initIntersection(
+    el,
+    ({ isIntersecting, time }) => {
+      if (isIntersecting) {
+        entryTime = time
+      } else if (entryTime) {
+        const diffTime = +((time - entryTime) / 1000).toFixed(1)
+        cb(diffTime + 's')
+        entryTime = 0
+      }
+    },
+    { threshold: viewElementProcent }
+  )
+}
+
+function visibilityEvent(el, cb, customConfig = {}) {
+  const config = {
+    threshold: 0.3,
+    ...customConfig,
+    timer: null
+  }
+  initIntersection(
+    el,
+    ({ isIntersecting, target }, observer) => {
+      // console.log(target, isIntersecting);
+      if (isIntersecting) {
+        config.timer = setTimeout(() => {
+          if (isElementInViewport(target)) {
+            cb()
+            observer.disconnect()
+          }
+        }, 3000)
+      } else {
+        clearTimeout(config.timer)
+      }
+    },
+    config
+  )
+}
+
+// Artificial delay
+function delay(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+// Check if element in viewport
+function isElementInViewport(selector) {
+  const el = typeof selector === 'string' ? _$(selector) : selector
+
+  if (!el) return false
+
+  const rect = el.getBoundingClientRect()
+  const windowHeight =
+    window.innerHeight || document.documentElement.clientHeight
+
+  return (
+    rect.top + rect.height * 0.3 < windowHeight &&
+    rect.bottom > rect.height * 0.3
+  )
+  // return (
+  //   rect.top >= 0 &&
+  //   rect.left >= 0 &&
+  //   rect.bottom <=
+  //     (window.innerHeight || document.documentElement.clientHeight) &&
+  //   rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  // );
+}
+
+// Shordcode for selectors
+function _$(selector, context = document) {
+  return context.querySelector(selector)
+}
+function _$$(selector, context = document, toSimpleArray = false) {
+  const arr = context.querySelectorAll(selector)
+
+  return toSimpleArray ? Array.from(arr) : arr
+}
+
+// GA 4 events
+function pushDataLayer(name = '', desc = '', type = '', loc = '') {
+  window.dataLayer = window.dataLayer || []
+
+  try {
+    const event = {
+      event: 'event-to-ga4',
+      event_name: name,
+      event_desc: desc,
+      event_type: type,
+      event_loc: loc
+    }
+
+    console.debug('** GA4 Event **', event)
+
+    if (!config.debug) {
+      dataLayer.push(event)
+    }
+  } catch (e) {
+    console.log('** GA4 Error **', e)
+  }
+}
+
+// *** Exp BG process *** //
+
+//Clarity
+if (
+  !config.debug &&
+  Array.isArray(config.clarity) &&
+  config.clarity.length === 3
+) {
+  waitFor(
+    () => typeof clarity == 'function',
+    () => {
+      clarity(...config.clarity)
+    }
+  )
+}
+
+// Svg objects
+function getSvg(name) {
+  const svgObj = {}
+
+  return svgObj[name]
+}
