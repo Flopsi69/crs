4560f71a80c8fc71d39615eb64c315919023dae2
Alexander Lytovchenko <flopsi69@gmail.com>
2026-07-06 17:03:00 +0700

    add new price logic

--- govplus/price/index.js
+++ govplus/price/index.js
@@ -1,4 +1,3 @@
-
 (function () {
   if (_$('.lav-pricing')) {
     console.log('** Exp check failed: lav-pricing exist **', _$('.lav-pricing'))
@@ -14,117 +13,49 @@
 
   // Styles for Experiment
   const styles = /* css */ `
-    .lav-pricing .traveling-soon {
-      display: none;
-    }
-    .lav-pricing {
-      flex-flow: row!important;
-      gap: 12px!important;
-    }
-    .charge-payment-form .payment-method-information.lav-pricing .payment-method-information__item .GSubscriptionMethodRadioButton {
-      padding: 24px 12px!important;
-      height: 100%;
-      border: 2px solid #D3E4FD!important;
-      background: #F5F9FF!important;
-    }
-    .charge-payment-form .payment-method-information.lav-pricing .payment-method-information__item .ant-radio-wrapper-checked {
-      border-color: #0338AB!important;
-      box-shadow: 0 8px 24px 4px rgba(0, 0, 0, 0.12)!important;
-      background-color: #E2EEFF!important;
-    }
-    .lav-pricing .payment-method-information__item .content__wrapper {
-      display: block;
-    }
-    .lav-pricing .GSubscriptionMethodRadioButton .content .sale-block {
-      align-items: center;
-    }
-    .lav-pricing .payment-method-information__item .content {
-      display: block;
-      margin: 0 !important;
-      padding: 0;
-    }
-    .lav-pricing .payment-method-information__item .content .sale-block__processing .GText {
-      display: none;
-    }
-    .lav-pricing .payment-method-information__item .content .sale-block__processing .ant-typography:nth-child(2) {
-      color: #191919!important;
-      text-align: center!important;
-      font-size: 17px!important;
+    .PaymentCombinedInformation .payment-method-information__item .content__wrapper .sale-block .sale-block__processing>div:last-child {
       font-weight: 500!important;
-      line-height: 25px!important;
       letter-spacing: -0.1px!important;
-      margin-top: 12px!important;
-      text-decoration: none!important;
       min-height: 50px;
       display: flex;
       align-items: center;
       justify-content: center;
     }
-    .dashboardFormContainer label.ant-radio-wrapper.ant-radio-wrapper-in-form-item.ant-radio-wrapper-checked .ant-radio {
-      background: #0338AB !important;
-    }
-    .lav-pricing .payment-method-information__item .ant-radio {
-      position: relative !important;
-      left: 0 !important;
-      top: 0 !important;
-      transform: none !important;
-      margin: auto;
-    }
     .lav-pricing .payment-method-information__item .content .price-block {
-      margin-top: 24px;
+      display: flex;
       justify-content: center;
     }
-    .lav-pricing .payment-method-information__item .content .price-block .ant-typography:nth-child(1) {
-      color: #3E3E3E!important;
-      font-size: 18px!important;
-      line-height: 25.2px!important;
-    }
-    .lav-pricing .payment-method-information__item .content .price-block .ant-typography:nth-child(2) {
-      color: #191919!important;
-      font-size: 20px!important;
-      font-weight: 600!important;
-      line-height: 26px!important;
-    }
-    .lav-title {
-      color: #191919;
-      text-align: center;
-      font-size: 36px;
-      font-weight: 600;
-      line-height: 46px;
-      letter-spacing: -1px;
-      margin-bottom: 25px;
-      padding-top: 12px;
+    .PaymentCombinedInformation .payment-method-information__item .content__wrapper .price-block .prices {
+      gap: 8px;
     }
     .lav-hero-title {
       max-width: 550px;
     }
+    .PaymentCombinedInformation .charge-payment-form .payment-method-information .payment-method-information__item .ant-radio-wrapper-checked .ant-radio {
+      background: #216DE3!important;
+    }
+    .charge-payment-form .payment-method-information.lav-pricing .payment-method-information__item .ant-radio-wrapper-checked {
+      border-color: #216DE3!important;
+    }
     @media(max-width: 767.98px) {
-      .lav-title {
-        padding-top: 0;
-        font-size: 24px;
-        line-height: 28px;
-        letter-spacing: -0.5px;
-      }
       .charge-payment-form .payment-footer p {
         text-align: center!important;
         font-size: 14px!important;
         letter-spacing: -0.2px!important;
       }
-      .lav-pricing {
-        flex-wrap: wrap!important;
-      }
-      .charge-payment-form .payment-method-information.lav-pricing .payment-method-information__item .GSubscriptionMethodRadioButton {
-        padding: 24px 16px !important;
-      }
-      .lav-pricing .payment-method-information__item {
-        flex: 1 1 40%;
+      .PaymentPageWrapper::before {
+        letter-spacing: -0.5px!important;
       }
       .lav-pricing .payment-method-information__item .content .price-block {
-        margin-top: 12px;
+        margin-top: -4px;
       }
       .lav-pricing .payment-method-information__item .content .price-block .prices {
         flex-direction: row;
-        gap: 6px;
+        gap: 6px!important;
+      }
+      .PaymentCombinedInformation .payment-method-information__item .content__wrapper .sale-block .sale-block__processing>div:last-child {
+        line-height: 25px!important;
+        font-size: 17px!important;
       }
     }
   `
@@ -167,9 +98,13 @@
     _$('.GHeader .GImageTitle h1 p').classList.add('lav-hero-title');
     _$('.GHeader .GImageTitle h1 p').innerText = 'Submit your passport renewal application';
 
-    _$('#itemPriceChargeId').insertAdjacentHTML('beforebegin', '<div class="lav-title">Get your passport within:</div>');
+    updateTextContent()
+    setTimeout(updateTextContent, 500);
 
-    _$$('.charge-payment-form .payment-method-information.lav-pricing .payment-method-information__item').forEach((el, index) => {
+  }
+
+  function updateTextContent() {
+   _$$('.charge-payment-form .payment-method-information.lav-pricing .payment-method-information__item').forEach((el, index) => {
       let text = "Regular Gov Timeline";
       if (index === 1) {
         text = "20 business days";
@@ -180,7 +115,6 @@
       }
       el.querySelector('.sale-block__processing .ant-typography:nth-child(2)').innerText = text;
     });
-
   }
 
   // *** HELPERS *** //
