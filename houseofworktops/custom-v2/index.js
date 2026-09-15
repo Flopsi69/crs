cdebba27269f78d0a817051e3acccc6b511f349c
Alexander Lytovchenko <flopsi69@gmail.com>
2026-09-15 16:00:33 +0700

    set debug mode to false in experiment configuration

--- houseofworktops/custom-v2/index.js
+++ houseofworktops/custom-v2/index.js
@@ -6,7 +6,7 @@
     // dir: 'http://127.0.0.1:5500/houseofworktops/sizeSelector',
     dir: 'https://flopsi69.github.io/crs/houseofworktops/custom-v2',
     clarity: ['set', 'exp_customization_feature', 'variant_1'],
-    debug: true,
+    debug: false,
     isDisableLayer: false,
     cutAnswered: false,
     oilingAnswered: false,
